8f915820cbc646dcb4ba494a88165614319ba2e9
Alan Smith <commit@alanwsmith.com>
2023-03-06 17:49:59 -0500

    Using charlie

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,24 @@ use std::fs;
 fn main() {
     let mut pb = PageBuilder::new();
 
-    let name = "bravo";
+    let name = "charlie";
 
     let mut input_path = dirs::home_dir().unwrap();
     input_path.push("workshop");
     input_path.push("neopolitan");
     input_path.push("samples");
-    input_path
-        .push(format!("{}.neo", name).as_str());
+    input_path.push(format!("{}.neo", name).as_str());
 
     let mut output_path = dirs::home_dir().unwrap();
     output_path.push("workshop");
     output_path.push("neopolitan-tests");
     output_path.push("site");
-    output_path
-        .push(format!("{}.html", name).as_str());
+    output_path.push(format!("{}.html", name).as_str());
 
-    pb.input = Some(
-        fs::read_to_string(input_path).unwrap(),
-    );
+    pb.input =
+        Some(fs::read_to_string(input_path).unwrap());
 
     fs::write(output_path, &pb.output());
 
-    println!("{}", (&pb.output()));
-
-    // for line in pb.content(&input_alfa) {
-    //     println!("{}", line);
-    // }
+    // println!("{}", (&pb.output()));
 }
