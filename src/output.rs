61b7733d1cebbe26dabbf37f495b29efe5879d18
Alan Smith <commit@alanwsmith.com>
2023-03-06 17:34:18 -0500

    Outputting a sample with the title

--- src/output.rs
+++ src/output.rs
@@ -4,15 +4,22 @@ use crate::page_builder::PageBuilder;
 impl PageBuilder {
     pub fn output(&self) -> String {
         let mut output: Vec<String> = vec![];
+        output.push(
+            "<!DOCTYPE html><html><body>"
+                .to_string(),
+        );
         for block in self.blocks() {
             if &block.0 == "title" {
-                output.push(format!(
-                    "<h1>{}</h1>",
-                    &block.1
-                ));
+                output.push(
+                    self.title_from(&block.1),
+                );
+            }
+            else {
+                dbg!(block);
             }
         }
+
+        output.push("</body></html>".to_string());
         output.join("\n")
-        // "asdf".to_string()
     }
 }
