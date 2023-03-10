3e18611addd38947d893442c94ae59fd481d6adb
Alan Smith <commit@alanwsmith.com>
2023-03-10 04:41:46 -0500

    making some progress

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-#![allow(dead_code)]
+#![allow(warnings)]
 use logos::Logos;
 // use std::fs;
 
 #[derive(Logos, Debug, PartialEq)]
 enum Token {
+    #[regex(r"-> P *\n *\n")]
+    Paragraph,
+
     #[regex(r"\w+")]
     Word,
 
@@ -15,46 +18,121 @@ enum Token {
 }
 
 #[derive(Debug)]
-struct Node {
-    wood: Wood,
+enum Node {
+    Page { children: Vec<Node> },
+    Paragraph { children: Vec<Node> },
+    TextSpan { text: String },
 }
 
+// #[derive(Debug)]
+// enum Node {
+//     Paragraph(Paragraph),
+//     Page(Page),
+//     TextSpan(TextSpan),
+// }
+
+// #[derive(Debug)]
+// enum Wood {
+//     Branch(Branch),
+//     Leaf(Leaf),
+// }
+
+// #[derive(Debug)]
+// enum Branch {
+//     Title(Title),
+//     Paragraph(Paragraph),
+// }
+
+// #[derive(Debug)]
+// enum Leaf {
+//     Text(Text),
+// }
+
+// #[derive(Debug)]
+// struct Title {}
+
+// #[derive(Debug)]
+// struct Paragraph {
+//     children: Vec<Node>,
+// }
+
+// #[derive(Debug)]
+// struct Word {}
+
+// #[derive(Debug)]
+// struct Space {}
+
+// #[derive(Debug)]
+// struct Page {
+//     children: Vec<Node>,
+// }
+
 #[derive(Debug)]
-enum Wood {
-    Root(Root),
-    Branch(Branch),
-    Leaf(Leaf),
+struct TextSpan {
+    text: String,
 }
-
-#[derive(Debug)]
-struct Root {}
-
-#[derive(Debug)]
-enum Branch {
-    Title(Title),
-    Paragraph(Paragraph),
-}
-
-#[derive(Debug)]
-enum Leaf {
-    Text(Text),
-}
-
-#[derive(Debug)]
-struct Title {}
-
-#[derive(Debug)]
-struct Paragraph {}
-
-#[derive(Debug)]
-struct Text {}
 
 fn main() {
     println!("Starting");
-    let input = "the quick brown fox";
-    let root_wood = Wood::Root(Root {});
-    let root_node = Node { wood: root_wood };
-    j
+    let input = "-> P\n\nthe quick brown fox";
+    let mut page = Node::Page { children: vec![] };
+    let mut lex = Token::lexer(&input);
+    parse_next(&mut lex, &mut page);
+    dbg!(page);
+    println!("Done.");
+}
+
+fn parse_next(
+    mut lex: &mut dyn Iterator<Item = Token>,
+    node: &mut Node,
+) {
+    match lex.next() {
+        Some(Token::Paragraph) => {
+            println!("paragraph");
+            let mut text_span = Node::TextSpan {
+                text: "".to_string(),
+            };
+            parse_next(&mut lex, &mut text_span);
+            let mut paragraph = Node::Paragraph {
+                children: vec![text_span],
+            };
+
+            // node.Paragraph.children.push(paragraph);
+
+            // match lex.peekable().peek() {
+            //     Some(Token::Word) => {
+            //         let mut text_span =
+            //             Node::TextSpan(TextSpan {
+            //                 text: "".to_string(),
+            //             });
+            //         parse_next(&mut lex, &mut text_span);
+            //     }
+            //     Some(Token::Space) => {
+            //         let mut text_span =
+            //             Node::TextSpan(TextSpan {
+            //                 text: "".to_string(),
+            //             });
+            //         parse_next(&mut lex, &mut text_span);
+            //     }
+            //     Some(_) => {}
+            //     None => {}
+            // }
+            // parse_next(&mut lex, &mut paragraph);
+        }
+
+        Some(Token::Word) => {
+            println!("word");
+        }
+        Some(Token::Space) => {
+            println!("space");
+        }
+        Some(Token::Error) => {
+            println!("error");
+        }
+        None => {
+            println!("Found last thing");
+        }
+    }
 }
 
 // fn parseit(
