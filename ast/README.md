06b68d513d1277e695528546ea185d2b28e0c071
Alan Smith <commit@alanwsmith.com>
2023-03-09 22:19:03 -0500

    hacking around

---
+++ ast/README.md
@@ -0,0 +1,3 @@
+This is where I'm working on designing
+the AST. I expect it'll end up with 
+a JSON schema at the end of the process.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use logos::Logos;
 use std::fs;
 
@@ -22,7 +23,26 @@ enum Token<'a> {
     Error,
 }
 
+#[derive(Debug)]
+struct Page {
+    content: Vec<Content>,
+}
+
+#[derive(Debug)]
+enum Content {
+    Title(Title),
+    Paragraph(Paragraph),
+}
+
+#[derive(Debug)]
+struct Title {}
+
+#[derive(Debug)]
+struct Paragraph {}
+
 fn main() {
+    println!("Starting");
+
     let input_alfa = fs::read_to_string(
         "_test_files/scratchpad/alfa.neo",
     )
@@ -31,11 +51,33 @@ fn main() {
     let tokens: Vec<_> =
         Token::lexer(&input_alfa).spanned().collect();
 
-    for token in tokens.iter() {
-        dbg!(&token);
+    // dbg!(tokens);
+
+    let current_index: usize = 0;
+    process_token(tokens, current_index);
+
+    // let mut p = Page { content: vec![] };
+
+    // for token in tokens.iter() {
+    //     // p.content.push(token)
+    //     // dbg!(&token);
+    // }
+    // dbg!(p);
+
+    println!("Done.");
+}
+
+fn process_token<T: std::fmt::Debug>(
+    tokens: Vec<T>, current_index: usize,
+) {
+    if current_index < tokens.len() {
+        let token = tokens.get(current_index);
+        dbg!(token);
+        let next_index = current_index + 1;
+        process_token(tokens, next_index);
     }
-
-    // dbg!(input_alfa);
-
-    println!("Hello, world!");
+    // }
+    // else {
+    //     println!("done.");
+    // }
 }
