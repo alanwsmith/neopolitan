b58da3d9694cca89c6ea352553b98f27d30aa1f9
Alan Smith <commit@alanwsmith.com>
2023-03-10 02:57:45 -0500

    Moving towards a loop

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,18 @@ enum Token<'a> {
 }
 
 #[derive(Debug)]
+struct Node {
+    kind: Kind,
+    children: Vec<Node>,
+}
+
+#[derive(Debug)]
+enum Kind {
+    Title(Title),
+    Paragraph(Paragraph),
+}
+
+#[derive(Debug)]
 struct Page {
     content: Vec<Content>,
 }
@@ -40,6 +52,14 @@ struct Title {}
 #[derive(Debug)]
 struct Paragraph {}
 
+fn parseit(lex: &mut dyn Iterator<Item = Token>) {
+    dbg!(lex.next());
+    dbg!(lex.next());
+    dbg!(lex.next());
+    dbg!(lex.next());
+    println!("HEREREERR");
+}
+
 fn main() {
     println!("Starting");
 
@@ -48,13 +68,21 @@ fn main() {
     )
     .unwrap();
 
-    let tokens: Vec<_> =
-        Token::lexer(&input_alfa).spanned().collect();
+    let mut lex = Token::lexer(&input_alfa).peekable();
+    parseit(&mut lex);
+
+    // while let Some(x) = lex.next() {
+    //     dbg!(x);
+    //     dbg!(lex.peek());
+    // }
+
+    // dbg!(lex.peek().unwrap());
+    // dbg!(lex.next());
+    // dbg!(lex.peek());
 
     // dbg!(tokens);
-
-    let current_index: usize = 0;
-    process_token(tokens, current_index);
+    // let current_index: usize = 0;
+    // process_token(tokens, current_index);
 
     // let mut p = Page { content: vec![] };
 
@@ -67,17 +95,22 @@ fn main() {
     println!("Done.");
 }
 
-fn process_token<T: std::fmt::Debug>(
-    tokens: Vec<T>, current_index: usize,
-) {
-    if current_index < tokens.len() {
-        let token = tokens.get(current_index);
-        dbg!(token);
-        let next_index = current_index + 1;
-        process_token(tokens, next_index);
-    }
-    // }
-    // else {
-    //     println!("done.");
-    // }
-}
+// fn process_token<T: std::fmt::Debug>(
+//     tokens: Vec<T>, current_index: usize,
+// ) {
+//     if current_index < tokens.len() {
+//         let token = tokens.get(current_index);
+//         let thing = token.unwrap();
+//         dbg!(thing);
+//         match thing {
+//             Token::TitleTag => println!("HERERER"),
+//             _ => println!("HEREEQWEQWE"),
+//         }
+//         let next_index = current_index + 1;
+//         process_token(tokens, next_index);
+//     }
+//     // }
+//     // else {
+//     //     println!("done.");
+//     // }
+// }
