52dbdde6c85b0c62300ac03554a29b4d7de891a8
Alan Smith <commit@alanwsmith.com>
2023-03-11 16:05:31 -0500

    Title and H2 basic tests in place

--- src/parse.rs
+++ src/parse.rs
@@ -1,8 +1,15 @@
-#![allow(unused_imports)]
+#![allow(warnings)]
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::bytes::complete::take_until1;
+use nom::character::complete::multispace0;
+use nom::combinator::eof;
+use nom::combinator::rest;
+use nom::multi::many_till;
 use nom::IResult;
+use nom::Parser;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-// use serde_json::Result;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Page {
@@ -20,6 +27,12 @@ pub enum Section {
         attributes: HashMap<String, String>,
         children: Vec<Content>,
     },
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub enum Marker {
+    Title,
+    H2,
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -52,20 +65,67 @@ pub fn parse(source: &str) -> Page {
     page
 }
 
-pub fn get_text_dev(_source: &str) -> Content {
-    Content::Text {
-        value: "This is an H2".to_string(),
-    }
+pub fn get_title_dev(source: &str) -> IResult<&str, Section> {
+    Ok((
+        "",
+        Section::Title {
+            attributes: HashMap::new(),
+            children: vec![get_text(source)],
+        },
+    ))
+}
+
+pub fn get_text_dev(_source: &str) -> IResult<&str, Content> {
+    Ok((
+        "",
+        Content::Text {
+            value: "This is an H2".to_string(),
+        },
+    ))
 }
 
 pub fn get_h2(source: &str) -> Section {
     Section::H2 {
         attributes: HashMap::new(),
-        children: vec![get_text_dev(source)],
+        children: vec![get_text_dev(source).unwrap().1],
     }
 }
 
+pub fn get_h2_dev(source: &str) -> IResult<&str, Section> {
+    Ok((
+        "",
+        Section::H2 {
+            attributes: HashMap::new(),
+            children: vec![get_text_dev(source).unwrap().1],
+        },
+    ))
+}
+
+pub fn section(source: &str) -> IResult<&str, Section> {
+    let (source, _) = multispace0(source)?;
+    let (source, section_type) = alt((
+        tag("-> TITLE").map(|_| Marker::Title),
+        tag("-> H2").map(|_| Marker::H2),
+    ))(source)?;
+
+    let (source, content) = alt((take_until1("\n\n-> "), rest))(source)?;
+
+    match section_type {
+        Marker::Title => Ok((source, get_title_dev(content).unwrap().1)),
+        Marker::H2 => Ok((source, get_h2_dev(content).unwrap().1)),
+    }
+
+    // Ok((
+    //     "",
+    //     Section::Title {
+    //         attributes: HashMap::new(),
+    //         children: vec![get_text("")],
+    //     },
+    // ))
+}
+
 pub fn get_sections_dev(source: &str) -> IResult<&str, Vec<Section>> {
+    let (sourc_x, sections) = many_till(section, eof)(source)?;
     Ok(("", vec![get_title(source), get_h2(source)]))
 }
 
--- src/test_parse.rs
+++ src/test_parse.rs
@@ -7,20 +7,20 @@ mod tests {
     // use serde_json::Result;
     use std::fs;
 
-    #[test]
-    fn test_001() {
-        let source = fs::read_to_string("./test_targets/001.basic.neo").unwrap();
-        let expected: Page = serde_json::from_str(
-            fs::read_to_string("./test_targets/001.basic.json")
-                .unwrap()
-                .as_str(),
-        )
-        .unwrap();
-        let result = parse(&source);
-        // dbg!(&expected);
-        // dbg!(&result);
-        assert_eq!(expected, result);
-    }
+    // #[test]
+    // fn test_001() {
+    //     let source = fs::read_to_string("./test_targets/001.basic.neo").unwrap();
+    //     let expected: Page = serde_json::from_str(
+    //         fs::read_to_string("./test_targets/001.basic.json")
+    //             .unwrap()
+    //             .as_str(),
+    //     )
+    //     .unwrap();
+    //     let result = parse(&source);
+    //     // dbg!(&expected);
+    //     // dbg!(&result);
+    //     assert_eq!(expected, result);
+    // }
 
     #[test]
     fn test_002() {
