ca2989fc8c1cc43641320480445a0f53cb302dbc
Alan Smith <commit@alanwsmith.com>
2023-03-13 00:56:43 -0400

    First attribute being pulled in

--- src/get_attributes.rs
+++ src/get_attributes.rs
@@ -2,27 +2,38 @@ use nom::bytes::complete::tag;
 use nom::bytes::complete::take_until1;
 use nom::character::complete::multispace0;
 use nom::character::complete::not_line_ending;
+use nom::combinator::eof;
+use nom::multi::many_till;
 use nom::IResult;
 // use std::collections::HashMap;
 
 // pub fn get_attributes(source: &str) -> IResult<&str, HashMap<String, String>> {
 pub fn get_attributes(source: &str) -> IResult<&str, Vec<(String, String)>> {
-    let mut attributes: Vec<(String, String)> = vec![];
+    let (_, attributes) = many_till(attribute_splitter, eof)(source)?;
+    // let mut attributes: Vec<(String, String)> = vec![];
 
-    // let mut attributes = HashMap::new();
+    // let (source, _) = multispace0(source)?;
+    // let (source, _) = tag("-> ")(source)?;
+    // let (source, key) = take_until1(":")(source)?;
+    // let (source, _) = tag(":")(source)?;
+    // let (source, _) = multispace0(source)?;
+    // let (_, value) = not_line_ending(source)?;
+
+    // attributes.push((key.to_string(), value.to_string()));
+
+    Ok(("", attributes.0))
+}
+
+pub fn attribute_splitter(source: &str) -> IResult<&str, (String, String)> {
     let (source, _) = multispace0(source)?;
     let (source, _) = tag("-> ")(source)?;
     let (source, key) = take_until1(":")(source)?;
     let (source, _) = tag(":")(source)?;
     let (source, _) = multispace0(source)?;
-    let (_, value) = not_line_ending(source)?;
-    // dbg!(&source);
+    let (source, value) = not_line_ending(source)?;
+
     // dbg!(&key);
-    // attributes.insert(key.to_string(), value.to_string());
+    // dbg!(&value);
 
-    attributes.push((key.to_string(), value.to_string()));
-
-    Ok(("", attributes))
+    Ok((source, (key.to_string(), value.to_string())))
 }
-
-// pub fn attribute_splitter(source: &str) -> IResult<&str,
