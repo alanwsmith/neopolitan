73bf69698f8406cba4884a2876dd2081f9cfaf03
Alan Smith <commit@alanwsmith.com>
2023-03-16 23:20:57 -0400

    Switched to using vec to assemble test string

--- src/section_test.rs
+++ src/section_test.rs
@@ -5,10 +5,8 @@ use std::collections::HashMap;
 
 #[test]
 fn test_get_structure_001() {
-    let source = r#"-> TITLE
+    let source = vec!["-> TITLE", "", "Alfa Bravo"].join("\n");
 
-Alfa Bravo
-"#;
     let expected = Section::TITLE {
         children: vec![Chunk::H1 {
             attributes: HashMap::new(),
@@ -17,6 +15,6 @@ Alfa Bravo
             }],
         }],
     };
-    let result = section(source).unwrap().1;
+    let result = section(source.as_str()).unwrap().1;
     assert_eq!(expected, result);
 }
