b07eb451a46dc3630c9fe5ebb37ce57fa3b7bf25
Alan Smith <commit@alanwsmith.com>
2023-03-17 16:28:11 -0400

    Cleanup

--- src/process_text.rs
+++ src/process_text.rs
@@ -28,42 +28,6 @@ use nom::IResult;
 use nom::Parser;
 use std::collections::HashMap;
 
-// fn text(source: &str) -> IResult<&str, Vec<Chunk>> {
-//     // dbg!(&source);
-//     let mut response: Vec<Chunk> = vec![];
-//     let (source, pretext) = alt((tuple((take_until("`"), rest)), tuple((rest, rest))))(source)?;
-//     response.push(Chunk::Text {
-//         value: pretext.0.to_string(),
-//     });
-//     if pretext.1.is_empty() {
-//         Ok((source, response))
-//     } else {
-//         let (source, current) = tag("`")(pretext.1)?;
-//         let (source, code) = take_until("`")(source)?;
-//         let (source, current) = tag("`")(source)?;
-//         let (source, language) = take_until("`")(source)?;
-//         let (source, current) = tag("`")(source)?;
-//         response.push(Chunk::InlineCode {
-//             value: Some(code.to_string()),
-//             language: Some(language.to_string()),
-//             attributes: None,
-//         });
-//         Ok((source, response))
-//     }
-// }
-
-// pub fn process_text(source: &str) -> IResult<&str, Vec<Chunk>> {
-//     let (source, containers) = many_till(text, eof)(source)?;
-//     let response = containers.0.concat();
-//     Ok(("", response))
-// }
-
-// pub fn process_text_dev(source: &str) -> IResult<&str, Vec<Chunk>> {
-//     let (source, containers) = many_till(text_dev, eof)(source)?;
-//     let response = containers.0.concat();
-//     Ok(("", response))
-// }
-
 pub fn process_text(source: &str) -> IResult<&str, Vec<Chunk>> {
     let (source, containers) = many_till(text, eof)(source)?;
     let response = containers.0.concat();
@@ -76,73 +40,6 @@ enum Target {
     Rest { pretext: String },
     Code { pretext: String },
 }
-
-// fn text_dev(source: &str) -> IResult<&str, Vec<Chunk>> {
-//     // dbg!(&source);
-//     let mut response: Vec<Chunk> = vec![];
-//     let (source, payload) = alt((
-//         tuple((
-//             take_until("<<link").map(|v: &str| Target::Link {
-//                 pretext: v.to_string(),
-//             }),
-//             rest,
-//         )),
-//         tuple((
-//             rest.map(|v: &str| Target::Rest {
-//                 pretext: v.to_string(),
-//             }),
-//             rest,
-//         )),
-//     ))(source)?;
-//     match payload.0 {
-//         Target::Code { pretext } => Ok((source, vec![Chunk::Placeholder])),
-//         Target::Link { pretext } => {
-//             response.push(Chunk::Text {
-//                 value: pretext.to_string(),
-//             });
-//             let (source, _) = tag("<<")(payload.1)?;
-//             let (source, kind) = take_until("|")(source)?;
-//             let (source, _) = tag("|")(source)?;
-//             let (source, value) = take_until("|")(source)?;
-//             let (source, _) = tag("|")(source)?;
-//             let (source, url) = take_until(">>")(source)?;
-//             let (source, _) = tag(">>")(source)?;
-//             response.push(Chunk::Link {
-//                 value: Some(value.to_string()),
-//                 url: Some(url.to_string()),
-//                 attributes: None,
-//             });
-//             Ok((source, response))
-//         }
-//         Target::Rest { pretext } => {
-//             response.push(Chunk::Text {
-//                 value: pretext.to_string(),
-//             });
-//             Ok((source, response))
-//         }
-//     }
-//     // response.push(Chunk::Text {
-//     //     value: pretext.0.to_string(),
-//     // });
-//     // if pretext.1.is_empty() {
-//     //     Ok((source, response))
-//     // } else {
-//     //     let (source, _) = tag("<<")(pretext.1)?;
-//     //     let (source, kind) = take_until("|")(source)?;
-//     //     let (source, _) = tag("|")(source)?;
-//     //     let (source, value) = take_until("|")(source)?;
-//     //     let (source, _) = tag("|")(source)?;
-//     //     let (source, url) = take_until(">>")(source)?;
-//     //     let (source, _) = tag(">>")(source)?;
-//     //     response.push(Chunk::Link {
-//     //         value: Some(value.to_string()),
-//     //         url: Some(url.to_string()),
-//     //         attributes: None,
-//     //     });
-//     //     Ok((source, response))
-//     // }
-//     // Ok((source, vec![Chunk::Placeholder]))
-// }
 
 fn text(source: &str) -> IResult<&str, Vec<Chunk>> {
     dbg!(&source);
