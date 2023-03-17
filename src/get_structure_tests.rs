d474ff131e657db93b92589e6fd3a081d5e4e952
Alan Smith <commit@alanwsmith.com>
2023-03-17 02:30:45 -0400

    Added integration test with paragraphs

--- src/get_structure_tests.rs
+++ src/get_structure_tests.rs
@@ -6,10 +6,7 @@ use std::collections::HashMap;
 
 #[test]
 fn test_get_structure_001() {
-    let source = r#"-> TITLE
-
-Alfa Bravo
-"#;
+    let source = vec!["-> TITLE", "", "Alfa Bravo"].join("\n");
     let expected = Page {
         attributes: HashMap::new(),
         children: vec![Section::TITLE {
@@ -21,6 +18,40 @@ Alfa Bravo
             }],
         }],
     };
-    let result = get_structure(source).unwrap().1;
+    let result = get_structure(source.as_str()).unwrap().1;
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn test_get_structure_002() {
+    let source = vec![
+        "-> TITLE",
+        "",
+        "Alfa Bravo",
+        "",
+        "The quick brown fox",
+        "and the lazy dog",
+    ]
+    .join("\n");
+    let expected = Page {
+        attributes: HashMap::new(),
+        children: vec![Section::TITLE {
+            children: vec![
+                Chunk::H1 {
+                    attributes: HashMap::from([("class".to_string(), "title".to_string())]),
+                    children: vec![Chunk::Text {
+                        value: "Alfa Bravo".to_string(),
+                    }],
+                },
+                Chunk::P {
+                    attributes: HashMap::from([]),
+                    children: vec![Chunk::Text {
+                        value: "The quick brown fox\nand the lazy dog".to_string(),
+                    }],
+                },
+            ],
+        }],
+    };
+    let result = get_structure(source.as_str()).unwrap().1;
     assert_eq!(expected, result);
 }
