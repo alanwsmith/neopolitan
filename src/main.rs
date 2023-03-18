bef09c061834ff72d91db698708441f3657ab063
Alan Smith <commit@alanwsmith.com>
2023-03-18 13:43:12 -0400

    Adding baseline sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// use neopolitan::page::Page;
+use neopolitan::get_structure::get_structure;
+use std::fs;
+use std::path::PathBuf;
+
 fn main() {
-    println!("hello world");
+    // hard coded path to be run from
+    // the Cargo.toml directory
+    let mut path = PathBuf::new();
+    path.push("src");
+    path.push("_sample");
+    path.set_extension("neo");
+    let source = fs::read_to_string(path).expect("Could not open file");
+    let structure = get_structure(source.as_str());
+    dbg!(&structure);
 }
