2ffd18ea374e3efe538ea75a9d7a7d7369dbd3b0
Alan Smith <commit@alanwsmith.com>
2023-03-24 00:58:18 -0400

    Updated everything to use option strings

--- src/chunk.rs
+++ src/chunk.rs
@@ -10,28 +10,28 @@ pub enum Chunk {
         children: Option<Vec<Chunk>>,
     },
     Text {
-        attributes: Option<HashMap<String, String>>,
+        attributes: Option<HashMap<String, Option<String>>>,
         value: Option<String>,
     },
     P {
-        attributes: Option<HashMap<String, String>>,
+        attributes: Option<HashMap<String, Option<String>>>,
         children: Option<Vec<Chunk>>,
     },
     ListItem {
-        attributes: Option<HashMap<String, String>>,
+        attributes: Option<HashMap<String, Option<String>>>,
         children: Option<Vec<Chunk>>,
     },
     Strong {
-        attributes: Option<HashMap<String, String>>,
+        attributes: Option<HashMap<String, Option<String>>>,
         value: Option<String>,
     },
     Link {
-        attributes: Option<HashMap<String, String>>,
+        attributes: Option<HashMap<String, Option<String>>>,
         value: Option<String>,
         url: Option<String>,
     },
     InlineCode {
-        attributes: Option<HashMap<String, String>>,
+        attributes: Option<HashMap<String, Option<String>>>,
         language: Option<String>,
         value: Option<String>,
     },
