4b0ce046fc55619a38e3a64129b1a7f006000060
Alan Smith <commit@alanwsmith.com>
2023-03-23 21:41:22 -0400

    Starting to setup attributes as hashmap

--- src/attributes.rs
+++ src/attributes.rs
@@ -1,3 +1,4 @@
+#![allow(warnings)]
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::bytes::complete::take_until;
@@ -6,65 +7,93 @@ use nom::combinator::rest;
 use nom::multi::many0;
 use nom::sequence::tuple;
 use nom::IResult;
+use std::collections::HashMap;
 
 #[derive(Debug, PartialEq)]
 pub struct Attributes {
     pub values: Option<Vec<(Option<String>, Option<String>)>>,
 }
 
-pub fn attributes_old(source: &str) -> IResult<&str, Attributes> {
+pub fn attributes_dev(source: &str) -> IResult<&str, Option<HashMap<String, Option<String>>>> {
     let (remainder, source) = take_until("\n\n")(source)?;
-    let (final_part, mut parts) = many0(part)(source)?;
-    parts.push(final_part);
-    let mut attribute_holder: Vec<(Option<String>, Option<String>)> = vec![];
-    for part in parts.iter().skip(1) {
-        let (_, b) = attribute(part)?;
-        attribute_holder.push(b);
-    }
-    let result = Attributes {
-        values: Some(attribute_holder),
-    };
-    Ok((remainder.trim(), result))
+    let (_, parts) = many0(part)(source)?;
+    let mut response: HashMap<String, Option<String>> = HashMap::new();
+    parts.iter().for_each(|p| {
+        let (key, value) = attribute(p).unwrap();
+        response.insert(key.to_string(), value);
+    });
+
+    // if final_part.is_empty() {
+    //     Ok(("", None))
+    // } else {
+    //     // parts.push(final_part);
+    //     // dbg!(&parts);
+    //     let mut response: HashMap<String, Option<String>> = HashMap::new();
+    //     response.insert("box".to_string(), Some("planks".to_string()));
+    //     Ok(("", Some(response)))
+    // }
+
+    // response.insert("box".to_string(), Some("planks".to_string()));
+    Ok(("", Some(response)))
+}
+
+pub fn attribute(source: &str) -> IResult<&str, Option<String>> {
+    let (remainder, key) = take_until(": ")(source)?;
+    let (value, _) = tag(": ")(remainder)?;
+    Ok((key, Some(value.to_string())))
 }
 
 pub fn attributes(source: &str) -> IResult<&str, Option<Vec<(Option<String>, Option<String>)>>> {
-    let (remainder, source) = take_until("\n\n")(source)?;
-    let (final_part, mut parts) = many0(part)(source)?;
-    if final_part.is_empty() {
-        parts.push(final_part);
-        let mut attribute_holder: Vec<(Option<String>, Option<String>)> = vec![];
-        for part in parts.iter().skip(1) {
-            let (_, b) = attribute(part)?;
-            attribute_holder.push(b);
-        }
-        Ok((remainder.trim(), None))
-    } else {
-        parts.push(final_part);
-        let mut attribute_holder: Vec<(Option<String>, Option<String>)> = vec![];
-        for part in parts.iter().skip(1) {
-            let (_, b) = attribute(part)?;
-            attribute_holder.push(b);
-        }
-        Ok((remainder.trim(), Some(attribute_holder)))
-    }
+    Ok(("", None))
 }
 
 fn part(source: &str) -> IResult<&str, &str> {
-    let (source, content) = take_until(">> ")(source)?;
-    let (source, _) = tag(">> ")(source)?;
-    Ok((source, content))
+    // dbg!(&source);
+    // dbg!("------------");
+    let (remainder, content) = tag(">> ")(source)?;
+    // dbg!(&content);
+    // dbg!(&remainder);
+    let (remainder, content) = alt((take_until("\n"), rest))(remainder)?;
+    // dbg!(&remainder);
+    // dbg!(&content);
+
+    // let (remainder, content) = take_until(">> ")(content)?;
+    // let (source, _) = tag(">> ")(source)?;
+    Ok((remainder, content))
 }
 
-pub fn attribute(source: &str) -> IResult<&str, (Option<String>, Option<String>)> {
-    let (v, k) = alt((tuple((take_until(":"), rest)), tuple((rest, rest))))(source)?;
-    if k.1.is_empty() {
-        Ok((v, (Some(k.0.trim().to_string()), None)))
-    } else {
-        let (v, _) = tag(":")(k.1)?;
-        let (v, _) = multispace0(v)?;
-        Ok((
-            v,
-            (Some(k.0.trim().to_string()), Some(v.trim().to_string())),
-        ))
-    }
-}
+// pub fn attribute(source: &str) -> IResult<&str, (Option<String>, Option<String>)> {
+//     let (v, k) = alt((tuple((take_until(":"), rest)), tuple((rest, rest))))(source)?;
+//     if k.1.is_empty() {
+//         Ok((v, (Some(k.0.trim().to_string()), None)))
+//     } else {
+//         let (v, _) = tag(":")(k.1)?;
+//         let (v, _) = multispace0(v)?;
+//         Ok((
+//             v,
+//             (Some(k.0.trim().to_string()), Some(v.trim().to_string())),
+//         ))
+//     }
+// }
+
+// pub fn attributes(source: &str) -> IResult<&str, Option<Vec<(Option<String>, Option<String>)>>> {
+//     let (remainder, source) = take_until("\n\n")(source)?;
+//     let (final_part, mut parts) = many0(part)(source)?;
+//     if final_part.is_empty() {
+//         parts.push(final_part);
+//         let mut attribute_holder: Vec<(Option<String>, Option<String>)> = vec![];
+//         for part in parts.iter().skip(1) {
+//             let (_, b) = attribute(part)?;
+//             attribute_holder.push(b);
+//         }
+//         Ok((remainder.trim(), None))
+//     } else {
+//         parts.push(final_part);
+//         let mut attribute_holder: Vec<(Option<String>, Option<String>)> = vec![];
+//         for part in parts.iter().skip(1) {
+//             let (_, b) = attribute(part)?;
+//             attribute_holder.push(b);
+//         }
+//         Ok((remainder.trim(), Some(attribute_holder)))
+//     }
+// }
--- src/test_attributes.rs
+++ src/test_attributes.rs
@@ -35,45 +35,49 @@ use std::collections::HashMap;
 #[test]
 fn basic_attributes() {
     let source = ">> box: planks\n\nThe salt breeze";
-    let expected = Some(vec![(Some("box".to_string()), Some("planks".to_string()))]);
-    let result = attributes(source);
+    // let expected = Some(vec![(Some("box".to_string()), Some("planks".to_string()))]);
+    let expected = Some(HashMap::from([(
+        "box".to_string(),
+        Some("planks".to_string()),
+    )]));
+    let result = attributes_dev(source);
     assert_eq!(expected, result.unwrap().1);
 }
 
-#[test]
-fn full_attributes_langauge_without_other_stuff() {
-    let source = ">> python >> creek: wild\n\nRight hand side";
-    let expected = Some(vec![
-        (Some("python".to_string()), None),
-        (Some("creek".to_string()), Some("wild".to_string())),
-    ]);
-    let result = attributes(source);
-    assert_eq!(expected, result.unwrap().1);
-}
+// #[test]
+// fn full_attributes_langauge_without_other_stuff() {
+//     let source = ">> python >> creek: wild\n\nRight hand side";
+//     let expected = Some(vec![
+//         (Some("python".to_string()), None),
+//         (Some("creek".to_string()), Some("wild".to_string())),
+//     ]);
+//     let result = attributes(source);
+//     assert_eq!(expected, result.unwrap().1);
+// }
 
-#[test]
-fn no_attributes() {
-    let source = "\n\nThe pine tree";
-    let expected = None;
-    let result = attributes(source);
-    assert_eq!(expected, result.unwrap().1);
-}
+// #[test]
+// fn no_attributes() {
+//     let source = "\n\nThe pine tree";
+//     let expected = None;
+//     let result = attributes(source);
+//     assert_eq!(expected, result.unwrap().1);
+// }
 
-#[test]
-fn check_return_values() {
-    let source = "\n\nThe pine tree\n\nRunning again";
-    let result = attributes(source);
-    assert_eq!("The pine tree\n\nRunning again", result.unwrap().0);
-}
+// #[test]
+// fn check_return_values() {
+//     let source = "\n\nThe pine tree\n\nRunning again";
+//     let result = attributes(source);
+//     assert_eq!("The pine tree\n\nRunning again", result.unwrap().0);
+// }
 
 // ////////////////////////////////////////////
 // // This is just a singular attribute instead
 // // of the collection of attributes like above
 
-#[test]
-fn language_without_other_attribute() {
-    let source = "rust";
-    let expected = (Some("rust".to_string()), None);
-    let result = attribute(source);
-    assert_eq!(expected, result.unwrap().1);
-}
+// #[test]
+// fn language_without_other_attribute() {
+//     let source = "rust";
+//     let expected = (Some("rust".to_string()), None);
+//     let result = attribute(source);
+//     assert_eq!(expected, result.unwrap().1);
+// }
