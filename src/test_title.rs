5cd7718994a6b8291caed658bed97e520e4b2592
Alan Smith <commit@alanwsmith.com>
2023-04-03 02:03:14 -0400

    Moved tests to their own file

---
+++ src/test_title.rs
@@ -0,0 +1,54 @@
+use crate::block::Block;
+use crate::content::Content;
+use crate::p::p;
+use crate::section::Section;
+use nom::character::complete::multispace0;
+use nom::combinator::eof;
+use nom::multi::many_till;
+use nom::IResult;
+
+#[test]
+fn dev_title_test() {
+    let source = "\nHello, World\n\nquick brown fox";
+    let expected = Ok((
+        "",
+        Section::Title {
+            attributes: None,
+            children: Some(vec![
+                Block::P {
+                    attributes: None,
+                    children: Some(vec![Content::Text {
+                        text: Some("Hello, World".to_string()),
+                    }]),
+                },
+                Block::P {
+                    attributes: None,
+                    children: Some(vec![Content::Text {
+                        text: Some("quick brown fox".to_string()),
+                    }]),
+                },
+            ]),
+        },
+    ));
+    let result = title(source);
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn basic_title_response() {
+    let source = "\nHello, World";
+    let expected = Ok((
+        "",
+        Section::Title {
+            attributes: None,
+            children: Some(vec![Block::P {
+                attributes: None,
+                children: Some(vec![Content::Text {
+                    text: Some("Hello, World".to_string()),
+                }]),
+            }]),
+        },
+    ));
+    let result = title(source);
+    assert_eq!(expected, result);
+}
--- src/title.rs
+++ src/title.rs
@@ -1,5 +1,3 @@
-use crate::block::Block;
-use crate::content::Content;
 use crate::p::p;
 use crate::section::Section;
 use nom::character::complete::multispace0;
@@ -17,50 +15,4 @@ pub fn title(source: &str) -> IResult<&str, Section> {
             children: Some(paragraphs.0),
         },
     ))
-}
-
-#[test]
-fn dev_title_test() {
-    let source = "\nHello, World\n\nquick brown fox";
-    let expected = Ok((
-        "",
-        Section::Title {
-            attributes: None,
-            children: Some(vec![
-                Block::P {
-                    attributes: None,
-                    children: Some(vec![Content::Text {
-                        text: Some("Hello, World".to_string()),
-                    }]),
-                },
-                Block::P {
-                    attributes: None,
-                    children: Some(vec![Content::Text {
-                        text: Some("quick brown fox".to_string()),
-                    }]),
-                },
-            ]),
-        },
-    ));
-    let result = title(source);
-    assert_eq!(expected, result);
-}
-
-#[test]
-fn basic_title_response() {
-    let source = "\nHello, World";
-    let expected = Ok((
-        "",
-        Section::Title {
-            attributes: None,
-            children: Some(vec![Block::P {
-                attributes: None,
-                children: Some(vec![Content::Text {
-                    text: Some("Hello, World".to_string()),
-                }]),
-            }]),
-        },
-    ));
-    let result = title(source);
-    assert_eq!(expected, result);
 }
