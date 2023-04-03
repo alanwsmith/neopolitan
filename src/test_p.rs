f74e64456905a561325d3afba4904d7e8c70b7ad
Alan Smith <commit@alanwsmith.com>
2023-04-03 02:53:42 -0400

    Added test for multiple lines

--- src/test_p.rs
+++ src/test_p.rs
@@ -33,3 +33,20 @@ fn no_lines_at_end() {
     let result = p(source);
     assert_eq!(expected, result);
 }
+
+#[test]
+fn multiple_lines() {
+    let lines = ["Split the log with a quick,", "sharp blow."].join("\n");
+    let source = lines.as_str();
+    let expected = Ok((
+        "",
+        Block::P {
+            attributes: None,
+            children: Some(vec![Content::Text {
+                text: Some("Split the log with a quick,\nsharp blow.".to_string()),
+            }]),
+        },
+    ));
+    let result = p(source);
+    assert_eq!(expected, result);
+}
