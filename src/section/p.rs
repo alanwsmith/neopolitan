04e8515303697561bcbb9012c85ecb54f236e75e
Alan Smith <commit@alanwsmith.com>
2023-04-04 14:42:44 -0400

    Cleanup

--- src/section/p.rs
+++ src/section/p.rs
@@ -26,20 +26,3 @@ pub fn p(source: &str) -> IResult<&str, Section> {
         },
     ))
 }
-
-// pub fn title(source: &str) -> Section {
-//     let (s, att_capture) = many0(preceded(tag(">> "), section_attribute))(source).unwrap();
-//     let attributes = if att_capture.is_empty() {
-//         None
-//     } else {
-//         Some(att_capture)
-//     };
-//     // still not sure this is the right way to go about this.
-//     let (a, _) = multispace0::<&str, Error<&str>>(s).unwrap();
-//     let (_, b) = many_till(block, eof)(a).unwrap();
-//     let children = if b.0.is_empty() { None } else { Some(b.0) };
-//     Section::Title {
-//         attributes,
-//         children,
-//     }
-// }
