16b77d1cf7e69202b2d999554f5d36ed1117249d
Alan Smith <commit@alanwsmith.com>
2023-04-10 15:31:36 -0400

    Setup attributes with just a key

--- src/section/section_attributes.rs
+++ src/section/section_attributes.rs
@@ -1,3 +1,4 @@
+use nom::branch::alt;
 use nom::bytes::complete::is_not;
 use nom::bytes::complete::tag;
 use nom::character::complete::line_ending;
@@ -6,6 +7,7 @@ use nom::character::complete::space1;
 use nom::combinator::opt;
 use nom::sequence::tuple;
 use nom::IResult;
+use nom::Parser;
 
 #[derive(Debug, PartialEq)]
 pub enum SectionAttribute {
@@ -15,7 +17,8 @@ pub enum SectionAttribute {
     },
 }
 
-pub fn section_attribute(source: &str) -> IResult<&str, SectionAttribute> {
+pub fn section_attribute_old(source: &str) -> IResult<&str, SectionAttribute> {
+    // return section_attribute_dev(source);
     let (a, b) = opt(tuple((
         is_not(":"),
         tag(":"),
@@ -45,4 +48,43 @@ fn test_section_attribute() {
     ));
     let result = section_attribute(source);
     assert_eq!(expected, result);
+}
+
+#[test]
+fn test_section_attribute_only_key() {
+    let lines = ["rust", ""].join("\n");
+    let source = lines.as_str();
+    let expected = Ok((
+        "",
+        SectionAttribute::Attribute {
+            key: Some("rust".to_string()),
+            value: None,
+        },
+    ));
+    let result = section_attribute(source);
+    assert_eq!(expected, result);
+}
+
+pub fn section_attribute(source: &str) -> IResult<&str, SectionAttribute> {
+    let (a, b) = alt((
+        tuple((is_not(":"), tag(":"), space1, not_line_ending, line_ending)).map(|x| (x.0, x.3)),
+        tuple((not_line_ending, line_ending)).map(|x| (x.0, "")),
+    ))(source)?;
+    if b.1.is_empty() {
+        Ok((
+            a,
+            SectionAttribute::Attribute {
+                key: Some(b.0.to_string()),
+                value: None,
+            },
+        ))
+    } else {
+        Ok((
+            a,
+            SectionAttribute::Attribute {
+                key: Some(b.0.to_string()),
+                value: Some(b.1.to_string()),
+            },
+        ))
+    }
 }
--- src/tests/foxtrot.rs
+++ src/tests/foxtrot.rs
@@ -5,6 +5,7 @@ use crate::block::block::*;
 // use crate::content::content::*;
 use crate::parse::parse;
 use crate::section::section::*;
+use crate::section::section_attributes::*;
 use crate::wrapper::wrapper::*;
 
 #[test]
@@ -14,6 +15,25 @@ fn foxtrot() {
     let expected = Wrapper::Page {
         children: Some(vec![Section::CodeSection {
             attributes: None,
+            children: Some(Block::CodeBlock {
+                children: Some("some code\nmore code".to_string()),
+            }),
+        }]),
+    };
+    let result = parse(source).unwrap().1;
+    assert_eq!(expected, result);
+}
+
+#[test]
+fn foxtrot2_attributes() {
+    let lines = vec!["-> code", ">> language: rust", "", "some code", "more code"].join("\n");
+    let source = lines.as_str();
+    let expected = Wrapper::Page {
+        children: Some(vec![Section::CodeSection {
+            attributes: Some(vec![SectionAttribute::Attribute {
+                key: Some("language".to_string()),
+                value: Some("rust".to_string()),
+            }]),
             children: Some(Block::CodeBlock {
                 children: Some("some code\nmore code".to_string()),
             }),
