abb6a3ae61a95559ae8c9278c58c40932a6d3239
Alan Smith <commit@alanwsmith.com>
2023-04-10 23:07:17 -0400

    Added headers

---
+++ src/section/h6.rs
@@ -0,0 +1,28 @@
+use crate::block::block::*;
+use crate::section::section::*;
+use crate::section::section_attributes::*;
+use nom::bytes::complete::tag;
+use nom::combinator::eof;
+use nom::multi::many0;
+use nom::multi::many_till;
+use nom::sequence::preceded;
+use nom::IResult;
+
+pub fn h6(source: &str) -> IResult<&str, Section> {
+    // dbg!(source);
+    let (source, att_capture) = many0(preceded(tag(">> "), section_attribute))(source).unwrap();
+    let attributes = if att_capture.is_empty() {
+        None
+    } else {
+        Some(att_capture)
+    };
+    let (source, b) = many_till(block, eof)(source.trim()).unwrap();
+    let children = if b.0.is_empty() { None } else { Some(b.0) };
+    Ok((
+        source,
+        Section::H6Section {
+            attributes,
+            children,
+        },
+    ))
+}
