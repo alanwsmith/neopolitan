4613f9b4d4db081aaf02d41b7b61350e3e1a9a92
Alan Smith <commit@alanwsmith.com>
2023-04-14 16:10:04 -0400

    Moved paragraph test into file

--- src/section/p.rs
+++ src/section/p.rs
@@ -1,6 +1,6 @@
 use crate::block::block::*;
-use crate::section::section::*;
 use crate::section::attributes_for_section::*;
+use crate::section::section::*;
 use nom::bytes::complete::tag;
 use nom::combinator::eof;
 use nom::multi::many0;
@@ -24,4 +24,73 @@ pub fn p(source: &str) -> IResult<&str, Section> {
             children,
         },
     ))
+}
+
+#[cfg(test)]
+
+mod test {
+    use crate::block::block::*;
+    use crate::content::content::*;
+    use crate::parse::parse;
+    use crate::section::section::*;
+    use crate::wrapper::wrapper::*;
+
+    #[test]
+    fn basic_p() {
+        let lines = vec![
+            "-> p",
+            "",
+            "the book cover",
+            "",
+            "random string",
+            "with content",
+        ]
+        .join("\n");
+        let source = lines.as_str();
+        let expected = Wrapper::Page {
+            children: Some(vec![
+                Section::Paragraphs {
+                    attributes: None,
+                    children: Some(vec![
+                        Block::P {
+                            children: Some(vec![
+                                Content::Text {
+                                    text: Some("the".to_string()),
+                                },
+                                Content::Space,
+                                Content::Text {
+                                    text: Some("book".to_string()),
+                                },
+                                Content::Space,
+                                Content::Text {
+                                    text: Some("cover".to_string()),
+                                },
+                            ]),
+                        },
+                        Block::P {
+                            children: Some(vec![
+                                Content::Text {
+                                    text: Some("random".to_string()),
+                                },
+                                Content::Space,
+                                Content::Text {
+                                    text: Some("string".to_string()),
+                                },
+                                Content::Space,
+                                Content::Text {
+                                    text: Some("with".to_string()),
+                                },
+                                Content::Space,
+                                Content::Text {
+                                    text: Some("content".to_string()),
+                                },
+                            ]),
+                        },
+                    ]),
+                },
+            ]),
+        };
+        let result = parse(source).unwrap().1;
+        assert_eq!(expected, result);
+    }
 }
