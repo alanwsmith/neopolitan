b0b67f63453d3fa64961690dc3c7f0749d91f08a
Alan Smith <commit@alanwsmith.com>
2023-04-13 20:09:08 -0400

    Removed debug messages

--- src/block/unordered_list_item.rs
+++ src/block/unordered_list_item.rs
@@ -10,18 +10,18 @@ use nom::sequence::tuple;
 use nom::IResult;
 
 pub fn unordered_list_item(source: &str) -> IResult<&str, Block> {
-    dbg!("DDDDD");
-    dbg!(source);
+    // dbg!("DDDDD");
+    // dbg!(source);
     // let (remainder, _) = multispace0(source)?;
     let (remainder, _) = tuple((multispace0, tag("- ")))(source)?;
-    dbg!("EEEEE");
-    dbg!(remainder);
+    // dbg!("EEEEE");
+    // dbg!(remainder);
     let (remainder, content) =
         many_till(unordered_list_content, alt((tag("\n\n"), eof)))(remainder)?;
-    dbg!("FFFFF");
-    dbg!(&remainder);
-    dbg!("GGGGG");
-    dbg!(&content);
+    // dbg!("FFFFF");
+    // dbg!(&remainder);
+    // dbg!("GGGGG");
+    // dbg!(&content);
     Ok((
         remainder,
         Block::UnorderedListItem {
@@ -32,15 +32,15 @@ pub fn unordered_list_item(source: &str) -> IResult<&str, Block> {
 }
 
 pub fn unordered_list_content(source: &str) -> IResult<&str, Block> {
-    dbg!("HHHHH");
-    dbg!(&source);
+    // dbg!("HHHHH");
+    // dbg!(&source);
     // let (a, _) = tuple((multispace0, tag("- ")))(source)?;
     let (remainder, content) =
         many_till(content, alt((peek(tag("\n\n-")), tag("\n\n"), eof)))(source)?;
-    dbg!("IIIII");
-    dbg!(&remainder);
-    dbg!("JJJJJ");
-    dbg!(&content);
+    // dbg!("IIIII");
+    // dbg!(&remainder);
+    // dbg!("JJJJJ");
+    // dbg!(&content);
     Ok((
         remainder,
         Block::P {
