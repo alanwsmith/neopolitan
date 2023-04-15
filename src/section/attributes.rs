676a67f7214d152cd816bf8729b651a34cb641d0
Alan Smith <commit@alanwsmith.com>
2023-04-15 12:44:19 -0400

    Cleanup

--- src/section/attributes.rs
+++ src/section/attributes.rs
@@ -7,7 +7,7 @@ use nom::sequence::preceded;
 use nom::IResult;
 
 pub fn attributes(source: &str) -> IResult<&str, Section> {
-    dbg!(source);
+    // dbg!(source);
     let (source, att_capture) = many0(preceded(tag(">> "), section_attribute))(source).unwrap();
     let attributes = if att_capture.is_empty() {
         None
