6be596e9f82f844977881a8d2d9069603c3a61ac
Alan Smith <commit@alanwsmith.com>
2023-04-15 17:09:05 -0400

    Fixed issue where some attributes weren't showing up

--- src/section/attributes_for_section.rs
+++ src/section/attributes_for_section.rs
@@ -1,10 +1,13 @@
 use nom::branch::alt;
 use nom::bytes::complete::is_not;
 use nom::bytes::complete::tag;
+//use nom::bytes::complete::take_till;
+use nom::bytes::complete::take_until;
 use nom::character::complete::line_ending;
 use nom::character::complete::not_line_ending;
 use nom::character::complete::space1;
 use nom::combinator::eof;
+use nom::combinator::rest;
 use nom::sequence::tuple;
 use nom::IResult;
 use nom::Parser;
@@ -17,6 +20,42 @@ pub enum SectionAttribute {
         key: Option<String>,
         value: Option<String>,
     },
+}
+
+pub fn section_attribute(source: &str) -> IResult<&str, SectionAttribute> {
+    let (a, b) = alt((tuple((take_until("\n"), tag("\n"))).map(|x| x.0), rest))(source)?;
+    // The above line was added to properly pull in
+    // secondary attributes. The bloew lines might have
+    // some kruft that's unnecessary now. Not worth
+    // removing unless it becomes a problem
+    let (_, b) = alt((
+        tuple((
+            is_not(":"),
+            tag(":"),
+            space1,
+            not_line_ending,
+            alt((line_ending, eof)),
+        ))
+        .map(|x| (x.0, x.3)),
+        tuple((not_line_ending, alt((line_ending, eof)))).map(|x| (x.0, "")),
+    ))(b)?;
+    if b.1.is_empty() {
+        Ok((
+            a.trim(),
+            SectionAttribute::Attribute {
+                key: Some(b.0.to_string()),
+                value: None,
+            },
+        ))
+    } else {
+        Ok((
+            a,
+            SectionAttribute::Attribute {
+                key: Some(b.0.to_string()),
+                value: Some(b.1.to_string()),
+            },
+        ))
+    }
 }
 
 #[test]
@@ -66,34 +105,19 @@ fn test_section_attribute_only_key_no_following_line() {
     assert_eq!(expected, result);
 }
 
-pub fn section_attribute(source: &str) -> IResult<&str, SectionAttribute> {
-    let (a, b) = alt((
-        tuple((
-            is_not(":"),
-            tag(":"),
-            space1,
-            not_line_ending,
-            alt((line_ending, eof)),
-        ))
-        .map(|x| (x.0, x.3)),
-        tuple((not_line_ending, alt((line_ending, eof)))).map(|x| (x.0, "")),
-    ))(source)?;
-    // dbg!(&b);
-    if b.1.is_empty() {
-        Ok((
-            a,
-            SectionAttribute::Attribute {
-                key: Some(b.0.to_string()),
-                value: None,
-            },
-        ))
-    } else {
-        Ok((
-            a,
-            SectionAttribute::Attribute {
-                key: Some(b.0.to_string()),
-                value: Some(b.1.to_string()),
-            },
-        ))
-    }
+#[test]
+fn single_first_item_followed_by_key_value() {
+    // This is for dealing with items taht get parsed out
+    // like youtube that only have attributes.
+    let lines = ["bravo", ">> charlie: delta"].join("\n");
+    let source = lines.as_str();
+    let expected = Ok((
+        ">> charlie: delta",
+        SectionAttribute::Attribute {
+            key: Some("bravo".to_string()),
+            value: None,
+        },
+    ));
+    let result = section_attribute(source);
+    assert_eq!(expected, result);
 }
