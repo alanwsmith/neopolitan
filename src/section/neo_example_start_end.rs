3dd6901bd2c22ee0558be9c3418beda92a91b07a
Alan Smith <commit@alanwsmith.com>
2023-04-15 12:12:24 -0400

    Starting to add the neoexample functionality

---
+++ src/section/neo_example_start_end.rs
@@ -0,0 +1,65 @@
+use crate::block::block::*;
+use crate::section::attributes_for_section::*;
+use crate::section::section::*;
+use nom::bytes::complete::tag;
+use nom::multi::many0;
+use nom::sequence::preceded;
+use nom::IResult;
+
+pub fn neo_example_start_end(source: &str) -> IResult<&str, Section> {
+    let (source, att_capture) = many0(preceded(tag(">> "), section_attribute))(source).unwrap();
+    let attributes = if att_capture.is_empty() {
+        None
+    } else {
+        Some(att_capture)
+    };
+    let children = if source.trim().is_empty() {
+        None
+    } else {
+        Some(Block::RawContent {
+            text: Some(source.trim().to_string()),
+        })
+    };
+    Ok((
+        source,
+        Section::NeoExampleStartEndSection {
+            attributes,
+            children,
+        },
+    ))
+}
+
+#[cfg(test)]
+
+mod test {
+    use crate::block::block::*;
+    use crate::parse::parse;
+    use crate::section::attributes_for_section::*;
+    use crate::section::section::*;
+    use crate::wrapper::wrapper::*;
+
+    #[test]
+    fn basic_code_start_end() {
+        let lines = vec![
+            "-> startneoexample",
+            "",
+            "startend",
+            "codeblock",
+            "",
+            "-> endneoexample",
+        ]
+        .join("\n");
+        let source = lines.as_str();
+        let expected = Wrapper::Page {
+            children: Some(vec![Section::NeoExampleStartEndSection {
+                attributes: None,
+                children: Some(Block::RawContent {
+                    text: Some("startend\ncodeblock".to_string()),
+                }),
+            }]),
+        };
+        let result = parse(source).unwrap().1;
+        assert_eq!(expected, result);
+    }
+
+}
