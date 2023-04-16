79b3898d98fcd293a059f0a88359d268400d5d00
Alan Smith <commit@alanwsmith.com>
2023-04-16 00:44:03 -0400

    Cleanup

--- src/parse.rs
+++ src/parse.rs
@@ -1,13 +1,13 @@
-use crate::wrapper::wrapper::Wrapper;
 use crate::section::section::*;
+use crate::wrapper::wrapper::Wrapper;
 use nom::combinator::eof;
 use nom::multi::many_till;
 use nom::IResult;
 
 pub fn parse(source: &str) -> IResult<&str, Wrapper> {
     let (_, sections) = many_till(section, eof)(source)?;
-    let expected = Wrapper::Page {
+    let page = Wrapper::Page {
         children: Some(sections.0),
     };
-    Ok(("", expected))
+    Ok(("", page))
 }
