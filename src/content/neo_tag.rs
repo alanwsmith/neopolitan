c708e36dd2becdea107036cb4ec4635d6adfacd7
Alan Smith <commit@alanwsmith.com>
2023-04-16 21:22:13 -0400

    Added code with stub for single key language call

--- src/content/neo_tag.rs
+++ src/content/neo_tag.rs
@@ -31,11 +31,15 @@ pub fn neo_tag_attributes(source: &str) -> IResult<&str, Option<Vec<Attribute>>>
 pub fn neo_tag<'a>(source: (&'a str, &'a str, &'a str)) -> IResult<&'a str, Content> {
     let (_, parts) = tuple((take_until("|"), tag("|"), rest))(source.1)?;
     let (a, b) = alt((
-        tuple((multispace0, tag("strong"), multispace0)).map(|_| Content::Strong {
+        tuple((multispace0, tag("b"), multispace0)).map(|_| Content::B {
             attributes: neo_tag_attributes(parts.2).unwrap().1,
             text: Some(parts.0.to_string()),
         }),
-        tuple((multispace0, tag("b"), multispace0)).map(|_| Content::B {
+        tuple((multispace0, tag("code"), multispace0)).map(|_| Content::Code {
+            attributes: neo_tag_attributes(parts.2).unwrap().1,
+            text: Some(parts.0.to_string()),
+        }),
+        tuple((multispace0, tag("strong"), multispace0)).map(|_| Content::Strong {
             attributes: neo_tag_attributes(parts.2).unwrap().1,
             text: Some(parts.0.to_string()),
         }),
@@ -52,7 +56,6 @@ mod test {
 
     #[test]
     fn b() {
-        // let lines = vec!["alfa <<bravo|strong|class: delta>> charlie"].join("\n");
         let source = ("", "alfa|b", "");
         let expected = Content::B {
             attributes: None,
@@ -64,7 +67,6 @@ mod test {
 
     #[test]
     fn b_with_one_attribute() {
-        // let lines = vec!["alfa <<bravo|strong|class: delta>> charlie"].join("\n");
         let source = ("", "bravo|b|class: charlie", "");
         let expected = Content::B {
             attributes: Some(vec![Attribute::Basic {
@@ -77,8 +79,72 @@ mod test {
         assert_eq!(expected, result.unwrap().1);
     }
 
-    // "<<b|delta>> <<b|echo>> <<b|foxtrot>>",
-    // "<<b|golf|class: advanced>>",
+    #[test]
+    fn b_with_two_attributes() {
+        let source = ("", "charlie|b|class: delta|id: echo", "");
+        let expected = Content::B {
+            attributes: Some(vec![
+                Attribute::Basic {
+                    key: Some("class".to_string()),
+                    value: Some("delta".to_string()),
+                },
+                Attribute::Basic {
+                    key: Some("id".to_string()),
+                    value: Some("echo".to_string()),
+                },
+            ]),
+            text: Some("charlie".to_string()),
+        };
+        let result = neo_tag(source);
+        assert_eq!(expected, result.unwrap().1);
+    }
+
+    #[test]
+    fn code() {
+        let source = ("", "foxtrot|code", "");
+        let expected = Content::Code {
+            attributes: None,
+            text: Some("foxtrot".to_string()),
+        };
+        let result = neo_tag(source);
+        assert_eq!(expected, result.unwrap().1);
+    }
+
+    #[ignore]
+    #[test]
+    fn code_with_single_key_for_language() {
+        let source = ("", "golf|code|rust", "");
+        let expected = Content::Code {
+            attributes: Some(vec![Attribute::Basic {
+                key: Some("class".to_string()),
+                value: Some("delta".to_string()),
+            }]),
+            text: Some("golf".to_string()),
+        };
+        let result = neo_tag(source);
+        assert_eq!(expected, result.unwrap().1);
+    }
+
+    #[test]
+    fn code_with_regular_attributes() {
+        let source = ("", "hotel|code|class: india|id: juliette", "");
+        let expected = Content::Code {
+            text: Some("hotel".to_string()),
+            attributes: Some(vec![
+                Attribute::Basic {
+                    key: Some("class".to_string()),
+                    value: Some("india".to_string()),
+                },
+                Attribute::Basic {
+                    key: Some("id".to_string()),
+                    value: Some("juliette".to_string()),
+                },
+            ]),
+        };
+        let result = neo_tag(source);
+        assert_eq!(expected, result.unwrap().1);
+    }
+
     // "<<code|quick brown>>",
     // "<<code|slow fox|lang: rust>>",
     // "<<code|slow fox|rust>>",
