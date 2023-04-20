f546905770dd7ec79cb707b1218660978d1a71ba
Alan Smith <commit@alanwsmith.com>
2023-04-20 00:59:59 -0400

    Handling classes with titles

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,3 +1,4 @@
 pub mod basic_title;
 pub mod title_with_attributes;
+pub mod title_with_class;
 pub mod title_with_paragraphs;
---
+++ src/tests/title_with_class.rs
@@ -0,0 +1,55 @@
+#![allow(warnings)]
+use crate::block::block::*;
+use crate::parse::parse::parse;
+use crate::section::section::*;
+use crate::source_file::source_file::SourceFile;
+use crate::universe::create_env::*;
+use crate::universe::universe::Universe;
+use minijinja::context;
+
+#[test]
+pub fn title_with_attributes() {
+    let mut u = Universe::new();
+    u.env = Some(create_env("./src/tests/templates"));
+    let lines = ["-> title", ">> class: echo", "Take the match"];
+    let expected = Some(vec![
+        r#"<h1 class="title echo">Take the match</h1>"#.to_string()
+    ]);
+    let source = lines.join("\n");
+    let mut sf = SourceFile::new();
+    sf.raw_data = Some(source.to_string());
+    sf.parsed = parse(sf.raw_data.unwrap().as_str()).unwrap().1;
+    sf.output_chunks = Some(vec![]);
+    sf.parsed.unwrap().iter().for_each(|section| {
+        match section {
+            Section::TitleSection {
+                attributes,
+                children,
+            } => {
+                let structure = u.env.as_ref().unwrap().get_template("title.j2").unwrap();
+                sf.output_chunks.as_mut().unwrap().push(
+                    structure
+                        .render(context!(attributes, children))
+                        .unwrap()
+                        .to_string(),
+                );
+            }
+            _ => {}
+        }
+        ()
+    });
+    let expected_string: String = expected
+        .unwrap()
+        .join("\n")
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect();
+    let output_string: String = sf
+        .output_chunks
+        .unwrap()
+        .join("\n")
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect();
+    assert_eq!(expected_string, output_string);
+}
