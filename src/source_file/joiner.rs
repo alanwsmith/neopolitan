222809920d7523eec73d64afd8735125729e8b0f
Alan Smith <commit@alanwsmith.com>
2023-04-20 14:44:55 -0400

    Using the joiner for the kdb

--- src/source_file/joiner.rs
+++ src/source_file/joiner.rs
@@ -4,13 +4,22 @@ use crate::snippet::snippet::Snippet;
 pub fn joiner(children: &Option<Vec<Block>>) -> Vec<String> {
     let mut joined: Vec<String> = vec![];
     children.as_ref().unwrap().iter().for_each(|block| {
-        let mut assembler = String::from("");
+        let mut assembler: Vec<String> = vec![];
         match block {
             Block::Text { snippets } => {
                 for snippet in snippets.as_ref().unwrap() {
                     match snippet {
                         Snippet::Plain { text } => {
-                            assembler.push_str(text.as_ref().unwrap());
+                            let new_thing = text.as_ref().unwrap();
+                            assembler.push(new_thing.to_string());
+                            ()
+                        }
+                        Snippet::Kbd { attributes, text } => {
+                            let mut content = String::from("<kbd");
+                            content.push_str(">");
+                            content.push_str(text.as_ref().unwrap());
+                            content.push_str("</kbd>");
+                            assembler.push(content.to_string());
                             ()
                         }
                         _ => (),
@@ -21,11 +30,10 @@ pub fn joiner(children: &Option<Vec<Block>>) -> Vec<String> {
             }
             _ => (),
         }
-        joined.push(assembler);
+        joined.push(assembler.join("\n"));
     });
 
     // dbg!(children);
     // "asdf".to_string()
     joined
 }
-
