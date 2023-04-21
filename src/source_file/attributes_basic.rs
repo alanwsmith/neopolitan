911bde675e1a04769ba7857ff28af237547df8c4
Alan Smith <commit@alanwsmith.com>
2023-04-21 12:09:50 -0400

    Setting up basic attributes in main process

--- src/source_file/attributes_basic.rs
+++ src/source_file/attributes_basic.rs
@@ -1,22 +1,25 @@
 use crate::section::section_attributes::SectionAttribute;
 
-pub fn attribute_basic(source: Option<Vec<SectionAttribute>>) -> Option<String> {
-    let mut response = String::from("");
-    source.unwrap().iter().for_each(|x| {
-        if let SectionAttribute::Attribute { key, value } = x {
-            response.push_str(
-                format!(
-                    r#" {}="{}""#,
-                    key.as_ref().unwrap(),
-                    value.as_ref().unwrap()
-                )
-                .as_str(),
-            );
+pub fn attributes_basic(source: &Option<Vec<SectionAttribute>>) -> Option<String> {
+    match source.as_ref() {
+        Some(attributes) => {
+            let mut response = String::from("");
+            attributes.iter().for_each(|x| {
+                if let SectionAttribute::Attribute { key, value } = x {
+                    response.push_str(
+                        format!(
+                            r#" {}="{}""#,
+                            key.as_ref().unwrap(),
+                            value.as_ref().unwrap()
+                        )
+                        .as_str(),
+                    );
+                }
+            });
+            Some(response)
         }
-    });
-
-    // Some(r#" id="foxtrot""#.to_string())
-    Some(response)
+        None => None,
+    }
 }
 
 #[cfg(test)]
@@ -24,12 +27,11 @@ mod test {
     use super::*;
     use crate::section::section_attributes::SectionAttribute;
 
-    #[ignore]
     #[test]
     fn empty() {
         let input: Option<Vec<SectionAttribute>> = None;
         let expected = None;
-        let results = attribute_basic(input);
+        let results = attributes_basic(&input);
         assert_eq!(expected, results);
     }
 
@@ -40,7 +42,7 @@ mod test {
             value: Some("foxtrot".to_string()),
         }]);
         let expected = Some(r#" id="foxtrot""#.to_string());
-        let results = attribute_basic(input);
+        let results = attributes_basic(&input);
         assert_eq!(expected, results);
     }
 }
