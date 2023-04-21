f19e51c6700d6dca70e6df7fc07831e8949300fd
Alan Smith <commit@alanwsmith.com>
2023-04-21 13:04:05 -0400

    Finished function to get title classes

--- src/source_file/attributes_with_class.rs
+++ src/source_file/attributes_with_class.rs
@@ -1,70 +1,5 @@
 use crate::section::section_attributes::SectionAttribute;
 
-pub fn attributes_with_class_old(
-    source: &Option<Vec<SectionAttribute>>,
-    _class_name: Option<String>,
-) -> Option<String> {
-    match source.as_ref() {
-        Some(attributes) => {
-            let mut response = String::from("");
-            attributes.iter().for_each(|x| {
-                if let SectionAttribute::Attribute { key, value } = x {
-                    response.push_str(
-                        format!(
-                            r#" {}="{}""#,
-                            key.as_ref().unwrap(),
-                            value.as_ref().unwrap()
-                        )
-                        .as_str(),
-                    );
-                }
-            });
-            Some(response)
-        }
-        None => None,
-    }
-}
-
-pub fn attributes_with_class_dev(
-    source: &Option<Vec<SectionAttribute>>,
-    class_name: Option<String>,
-) -> Option<String> {
-    match class_name {
-        Some(name) => {
-            let response = format!(r#" class="{}""#, name.as_str());
-            Some(response)
-        }
-        None => match source.as_ref() {
-            Some(attributes) => {
-                let mut response = String::from("");
-                attributes.iter().for_each(|x| {
-                    if let SectionAttribute::Attribute { key, value } = x {
-                        response.push_str(
-                            format!(
-                                r#" {}="{}""#,
-                                key.as_ref().unwrap(),
-                                value.as_ref().unwrap()
-                            )
-                            .as_str(),
-                        );
-                    }
-                });
-                Some(response)
-            }
-            None => None,
-        },
-    }
-}
-
-pub fn attributes_with_class_dev_2(
-    _source: &Option<Vec<SectionAttribute>>,
-    class_name: &str,
-) -> Option<String> {
-    let mut class_string = format!(r#" class="{}"#, class_name);
-    class_string.push_str(r#"""#);
-    Some(format!("{}", class_string))
-}
-
 pub fn attributes_with_class(
     source: &Option<Vec<SectionAttribute>>,
     class_name: &str,
@@ -75,14 +10,19 @@ pub fn attributes_with_class(
         Some(v) => {
             v.iter().for_each(|x| {
                 if let SectionAttribute::Attribute { key, value } = x {
-                    response.push_str(
-                        format!(
-                            r#" {}="{}""#,
-                            key.as_ref().unwrap(),
-                            value.as_ref().unwrap()
-                        )
-                        .as_str(),
-                    );
+                    if key.as_ref().unwrap() == "class" {
+                        class_string.push_str(" ");
+                        class_string.push_str(value.as_ref().unwrap().as_str());
+                    } else {
+                        response.push_str(
+                            format!(
+                                r#" {}="{}""#,
+                                key.as_ref().unwrap(),
+                                value.as_ref().unwrap()
+                            )
+                            .as_str(),
+                        );
+                    }
                 }
             });
         }
@@ -90,7 +30,6 @@ pub fn attributes_with_class(
     }
     class_string.push_str(r#"""#);
     response.push_str(class_string.as_str());
-    // Some(format!(r#" id="foxtrot"{}"#, class_string))
     Some(response)
 }
 
@@ -102,8 +41,8 @@ mod test {
     #[test]
     fn no_attributes_just_class() {
         let input: Option<Vec<SectionAttribute>> = None;
-        let expected: Option<String> = Some(String::from(r#" class="title""#));
-        let results = attributes_with_class(&input, "title");
+        let expected: Option<String> = Some(String::from(r#" class="alfa""#));
+        let results = attributes_with_class(&input, "alfa");
         assert_eq!(expected, results);
     }
 
@@ -111,22 +50,27 @@ mod test {
     fn attributes_without_extra_class() {
         let input: Option<Vec<SectionAttribute>> = Some(vec![SectionAttribute::Attribute {
             key: Some("id".to_string()),
-            value: Some("foxtrot".to_string()),
+            value: Some("bravo".to_string()),
         }]);
-        let expected = Some(r#" id="foxtrot" class="title""#.to_string());
-        let results = attributes_with_class(&input, "title");
+        let expected = Some(r#" id="bravo" class="charlie""#.to_string());
+        let results = attributes_with_class(&input, "charlie");
         assert_eq!(expected, results);
     }
 
-    #[ignore]
     #[test]
     fn class_in_attributes() {
-        let input: Option<Vec<SectionAttribute>> = Some(vec![SectionAttribute::Attribute {
-            key: Some("id".to_string()),
-            value: Some("foxtrot".to_string()),
-        }]);
-        let expected = Some(r#" id="foxtrot" class="title""#.to_string());
-        let results = attributes_with_class(&input, "title");
+        let input: Option<Vec<SectionAttribute>> = Some(vec![
+            SectionAttribute::Attribute {
+                key: Some("id".to_string()),
+                value: Some("delta".to_string()),
+            },
+            SectionAttribute::Attribute {
+                key: Some("class".to_string()),
+                value: Some("echo".to_string()),
+            },
+        ]);
+        let expected = Some(r#" id="delta" class="foxtrot echo""#.to_string());
+        let results = attributes_with_class(&input, "foxtrot");
         assert_eq!(expected, results);
     }
 }
