5c85b2f5c7a5fba394d2ec296e378da531219912
Alan Smith <commit@alanwsmith.com>
2023-04-22 18:40:48 -0400

    Update canvas.rs

--- src/section/canvas.rs
+++ src/section/canvas.rs
@@ -21,43 +21,41 @@ pub fn canvas(source: &str) -> IResult<&str, Section> {
 
 #[cfg(test)]
 mod test {
+    use crate::parse::parse::*;
+    use crate::source_file::source_file::*;
+    use crate::tests::remove_whitespace::remove_whitespace;
+    use crate::universe::create_env::create_env;
+    use crate::universe::universe::Universe;
 
-    // use crate::parse::parse::*;
-    // use crate::source_file::source_file::*;
-    // use crate::tests::remove_whitespace::remove_whitespace;
-    // use crate::universe::create_env::create_env;
-    // use crate::universe::universe::Universe;
-
-    // #[test]
-    // pub fn core_test_canvas() {
-    //     let source = [
-    //         "-> canvas",
-    //         ">> id: alfa",
-    //         ">> class: bravo",
-    //         "",
-    //         "Hold the hammer",
-    //         "",
-    //         "Heave the line",
-    //     ]
-    //     .join("\n")
-    //     .to_string();
-    //     let expected = Some(
-    //         vec![
-    //             r#"<canvas class="delta" id="bravo">"#,
-    //             r#"<p>Hold the hammer</p>"#,
-    //             r#"<p>Heave the line</p>"#,
-    //             r#"</canvas>"#,
-    //         ]
-    //         .join("\n")
-    //         .to_string(),
-    //     );
-    //     let mut u = Universe::new();
-    //     u.env = Some(create_env("./site/templates"));
-    //     let mut sf = SourceFile::new();
-    //     sf.raw = Some(source);
-    //     sf.parsed = parse(sf.raw.as_ref().unwrap().as_str()).unwrap().1;
-    //     let output = sf.output(&u);
-    //     assert_eq!(remove_whitespace(expected), remove_whitespace(output),);
-    // }
-
+    #[test]
+    pub fn two_attribute_on_canvas() {
+        let source = [
+            "-> canvas",
+            ">> class: delta",
+            ">> id: bravo",
+            "",
+            "Hold the hammer",
+            "",
+            "Heave the line",
+        ]
+        .join("\n")
+        .to_string();
+        let expected = Some(
+            vec![
+                r#"<canvas class="delta" id="bravo">"#,
+                r#"<p>Hold the hammer</p>"#,
+                r#"<p>Heave the line</p>"#,
+                r#"</canvas>"#,
+            ]
+            .join("\n")
+            .to_string(),
+        );
+        let mut u = Universe::new();
+        u.env = Some(create_env("./site/templates"));
+        let mut sf = SourceFile::new();
+        sf.raw = Some(source);
+        sf.parsed = parse(sf.raw.as_ref().unwrap().as_str()).unwrap().1;
+        let output = sf.output(&u);
+        assert_eq!(remove_whitespace(expected), remove_whitespace(output),);
+    }
 }
