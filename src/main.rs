ee6f159ac5922b4183d82334fcf6505d9bfafd31
Alan Smith <commit@alanwsmith.com>
2023-04-22 13:50:17 -0400

    Loading assets

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use neopolitan::helpers::load_assets::load_assets;
 use neopolitan::universe::create_env::create_env;
 use neopolitan::universe::universe::Universe;
+// use std::path::Path;
 use std::path::PathBuf;
 
 fn main() {
     println!("Starting build");
     let templates_dir = "./site/templates";
-    let assets_dir = "./site/assets";
+    let assets_dir = "./site/assets/";
     let content_dir = "./site/content";
     let build_dir = "./site/build";
-    load_assets(PathBuf::from(assets_dir), PathBuf::from(build_dir)).unwrap();
+    load_assets(assets_dir, build_dir).unwrap();
 
     let mut u = Universe::new();
     u.env = Some(create_env(templates_dir));
