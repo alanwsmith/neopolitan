ec968b611345bb96198e4cfd80271ad8fac9f005
Alan Smith <commit@alanwsmith.com>
2023-04-25 16:22:47 -0400

    Switched aside to use direct check for test

--- src/section/aside.rs
+++ src/section/aside.rs
@@ -21,41 +21,60 @@ pub fn aside(source: &str) -> IResult<&str, Section> {
 
 #[cfg(test)]
 mod test {
-    use crate::parse::parse::*;
-    use crate::source_file::source_file::*;
-    use crate::tests::remove_whitespace::remove_whitespace;
-    use crate::universe::create_env::create_env;
-    use crate::universe::universe::Universe;
+    use super::*;
+    use crate::snippet::snippet_enum::*;
+    // use crate::section::section::*;
+    // use crate::section::section_attributes::*;
+
+    // use crate::parse::parse::*;
+    // use crate::source_file::source_file::*;
+    // use crate::tests::remove_whitespace::remove_whitespace;
+    // use crate::universe::create_env::create_env;
+    // use crate::universe::universe::Universe;
 
     #[test]
     pub fn two_attribute_on_aside() {
-        let source = [
-            "-> aside",
-            ">> class: delta",
-            ">> id: bravo",
-            "",
-            "Hold the hammer",
-            "",
-            "Heave the line",
-        ]
-        .join("\n")
-        .to_string();
-        let expected = Some(
-            vec![
-                r#"<aside class="delta" id="bravo">"#,
-                r#"<p>Hold the hammer</p>"#,
-                r#"<p>Heave the line</p>"#,
-                r#"</aside>"#,
-            ]
+        let source = [">> class: delta", ">> id: bravo", "", "Run the race"]
             .join("\n")
-            .to_string(),
-        );
-        let mut u = Universe::new();
-        u.env = Some(create_env("./site/templates"));
-        let mut sf = SourceFile::new();
-        sf.raw = Some(source);
-        sf.parsed = parse(sf.raw.as_ref().unwrap().as_str()).unwrap().1;
-        let output = sf.output(&u);
-        assert_eq!(remove_whitespace(expected), remove_whitespace(output),);
+            .to_string();
+
+        let expected = Section::AsideSection {
+            attributes: Some(vec![
+                SectionAttribute::Attribute {
+                    key: Some("class".to_string()),
+                    value: Some("delta".to_string()),
+                },
+                SectionAttribute::Attribute {
+                    key: Some("id".to_string()),
+                    value: Some("bravo".to_string()),
+                },
+            ]),
+            children: Some(vec![Block::Text {
+                snippets: Some(vec![Snippet::Plain {
+                    text: Some("Run the race".to_string()),
+                }]),
+            }]),
+            // attributes_string: Some(r#" title="Some new title""#.to_string()),
+        };
+        let results = aside(&source).unwrap().1;
+        assert_eq!(expected, results);
+
+        // let expected = Some(
+        //     vec![
+        //         r#"<aside class="delta" id="bravo">"#,
+        //         r#"<p>Hold the hammer</p>"#,
+        //         r#"<p>Heave the line</p>"#,
+        //         r#"</aside>"#,
+        //     ]
+        //     .join("\n")
+        //     .to_string(),
+        // );
+        // let mut u = Universe::new();
+        // u.env = Some(create_env("./site/templates"));
+        // let mut sf = SourceFile::new();
+        // sf.raw = Some(source);
+        // sf.parsed = parse(sf.raw.as_ref().unwrap().as_str()).unwrap().1;
+        // let output = sf.output(&u);
+        // assert_eq!(remove_whitespace(expected), remove_whitespace(output),);
     }
 }
