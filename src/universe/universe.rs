d138b09bc3744e1c3905513c22310c347254e6ec
Alan Smith <commit@alanwsmith.com>
2023-05-02 17:47:44 -0400

    Showing total files output

--- src/universe/universe.rs
+++ src/universe/universe.rs
@@ -91,16 +91,16 @@ impl Universe<'_> {
 impl Universe<'_> {
     pub fn output_files(&self) {
         println!("Outputting files");
-        // let mut counter: u32 = 0;
+        let mut counter: u32 = 0;
         for (source_path, _source_file) in self.content_files.iter() {
             self.output_file(source_path.to_path_buf());
-            // counter += 1;
-            // if counter % 100 == 0 {
-            //     println!("Count: {}", counter);
-            // }
+            counter += 1;
+            if counter % 100 == 0 {
+                println!("Count: {}", counter);
+            }
         }
-        // println!("Count: {}", counter);
         println!("Output finished");
+        println!("Total Files: {}", counter);
     }
 }
 
