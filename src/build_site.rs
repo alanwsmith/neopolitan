5983814cd152808596c7067b3c2300539a3db656
Alan Smith <commit@alanwsmith.com>
2023-06-20 13:24:45 -0400

    Outputting stubbed index file from template

--- src/build_site.rs
+++ src/build_site.rs
@@ -1,11 +1,11 @@
 // #![allow(warnings)]
-// use minijinja::context;
-// use minijinja::Environment;
-// use minijinja::Source;
+use minijinja::context;
+use minijinja::Environment;
+use minijinja::Source;
+use std::path::PathBuf;
 // use crate::source_file::source_file::SourceFile;
 // use crate::files::files::Files;
 // use std::fs;
-// use std::path::PathBuf;
 //use walkdir::Error;
 // use walkdir::WalkDir;
 
@@ -14,36 +14,14 @@ use std::fs;
 pub fn build_site() {
     println!("Making the site");
     fs::create_dir("site").unwrap();
-
-    let source = r#"
-        <!DOCTYPE html>
-        <html>
-            <head><title>Alfa Bravo</title></head>
-        </html>"#;
-
-    fs::write("site/index.html", source).unwrap();
-
-    // let mut content = Files {
-    //     files: vec![],
-    //     content_dir: None,
-    // };
-    // let mut env = Environment::new();
-    // env.set_source(Source::from_path(PathBuf::from("./templates")));
-    // let wrapper = env.get_template("home_page.j2").unwrap();
-    // content.content_dir = Some(PathBuf::from("./content"));
-    // let _ = content.load_files();
-    // content.files.iter().for_each(|file| {
-    //     let mut output_path = PathBuf::from("./site");
-    //     output_path.push(file.raw_path.as_ref().unwrap());
-    //     output_path.set_extension("html");
-    //     let output = wrapper
-    //         .render(context!(
-    //             content => "ALFA BRAVO CHARLIE",
-    //             posts => content.posts(),
-    //         ))
-    //         .unwrap()
-    //         .to_string();
-    //     fs::write(output_path, output).unwrap();
-    //     ()
-    // });
+    let mut env = Environment::new();
+    env.set_source(Source::from_path(PathBuf::from("./templates")));
+    let wrapper = env.get_template("home_page.j2").unwrap();
+    let output = wrapper
+        .render(context!(
+            title => "ALFA BRAVO CHARLIE",
+        ))
+        .unwrap()
+        .to_string();
+    fs::write("site/index.html", output).unwrap();
 }
