ec871f650cab76b7788ede9406ed5530174a817e
Alan Smith <commit@alanwsmith.com>
2023-06-20 19:24:18 -0400

    Outputting with new directory method

--- src/build_site.rs
+++ src/build_site.rs
@@ -3,52 +3,56 @@ use crate::source_file::source_file::SourceFile;
 use minijinja::context;
 use minijinja::Environment;
 use minijinja::Source;
-use std::env;
 use std::fs;
 use std::path::PathBuf;
 use walkdir::WalkDir;
 
 pub fn build_site() {
     println!("Making the site");
-    fs::create_dir("site").unwrap();
+
+    let template_dir = PathBuf::from("templates");
+    let content_dir = PathBuf::from("content");
+    let site_root_dir = PathBuf::from("site");
+
+    // fs::create_dir(site_root_dir).unwrap();
+    //
 
     let mut env = Environment::new();
-    env.set_source(Source::from_path(PathBuf::from("./templates")));
+    env.set_source(Source::from_path(template_dir));
     let wrapper = env.get_template("home_page.j2").unwrap();
 
     let mut source_files: Vec<SourceFile> = vec![];
 
-    // TODO: Remove this when the path is being
-    // built in the prod locaiton
-    // fs::create_dir_all("site/posts/alfa").unwrap();
-
-    // Load the files
-    env::set_current_dir("content").unwrap();
-    for entry in WalkDir::new("./").into_iter() {
-        let p = entry.unwrap().path().to_path_buf();
-        if let Some(ext) = p.extension() {
+    for entry in WalkDir::new(&content_dir).into_iter() {
+        let initial_path = entry.unwrap().path().to_path_buf();
+        if let Some(ext) = initial_path.extension() {
             if ext == "neo" {
                 let mut sf = SourceFile::new();
-                sf.source_path = Some(p.clone());
-                sf.source_data = Some(fs::read_to_string(p).unwrap());
+                sf.source_path = Some(
+                    initial_path
+                        .clone()
+                        .strip_prefix(&content_dir)
+                        .unwrap()
+                        .to_path_buf(),
+                );
+                sf.source_data = Some(fs::read_to_string(initial_path).unwrap());
                 source_files.push(sf);
             }
         }
     }
 
     // Output the files
-    env::set_current_dir("../site").unwrap();
     source_files.iter().for_each(|source_file| {
-        // dbg!(source_file);
         let output = wrapper
             .render(context!(
                 title => source_file.title(),
             ))
             .unwrap()
             .to_string();
-        fs::create_dir_all(source_file.output_dir().unwrap()).unwrap();
-        fs::write(source_file.output_path().unwrap(), output).unwrap();
+        let mut file_path = site_root_dir.clone();
+        file_path.push(source_file.output_path().unwrap());
+        let dir_path = file_path.parent().unwrap();
+        fs::create_dir_all(dir_path).unwrap();
+        fs::write(file_path, output).unwrap();
     });
-
-    env::set_current_dir("..").unwrap();
 }
--- src/clear_output_directory.rs
+++ src/clear_output_directory.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::path::PathBuf;
 
 pub fn clear_output_directory() {
-    let output_dir = PathBuf::from("site");
+    let output_dir = PathBuf::from("./site");
     if output_dir.exists() {
         fs::remove_dir_all(output_dir).unwrap();
     }
