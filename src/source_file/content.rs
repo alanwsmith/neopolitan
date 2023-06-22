96f6c4fa3ac26ddd591c293eefc4a09f586593a1
Alan Smith <commit@alanwsmith.com>
2023-06-22 01:57:58 -0400

    Ignoring hidden sections

--- src/source_file/content.rs
+++ src/source_file/content.rs
@@ -14,49 +14,53 @@ use nom::Parser;
 
 impl SourceFile {
     pub fn content(&self) -> Option<String> {
-        let (_, b) = self.parse_content().unwrap();
+        let (_, b) = self.parse_content_dev().unwrap();
         b
     }
 
-    fn parse_content(&self) -> IResult<&str, Option<String>> {
+    fn parse_content_dev(&self) -> IResult<&str, Option<String>> {
         let (_, b) = many_till(
-            tuple((
-                multispace0,
-                tag_no_case("-> "),
-                alt((
-                    tuple((
-                        tag_no_case("title"),
+            alt((
+                tuple((
+                    multispace0,
+                    tag_no_case("-> "),
+                    alt((tuple((
+                        // these don't show up in the output
+                        alt((
+                            tag_no_case("attributes"),
+                            tag_no_case("categories"),
+                            tag_no_case("hidden"),
+                        )),
                         not_line_ending,
                         line_ending,
                         alt((take_until("\n\n-> "), rest)),
                     ))
-                    .map(|t| self.title_section(t.3)),
-                    tuple((
-                        tag_no_case("p"),
-                        not_line_ending,
-                        line_ending,
-                        alt((take_until("\n\n-> "), rest)),
-                    ))
-                    .map(|t| self.p_section(t.3)),
-                    tuple((
-                        tag_no_case("categories"),
-                        not_line_ending,
-                        line_ending,
-                        alt((take_until("\n\n-> "), rest)),
-                    ))
-                    .map(|_| Ok(("", Some("".to_string())))),
-                    tuple((
-                        tag_no_case("attributes"),
-                        not_line_ending,
-                        line_ending,
-                        alt((take_until("\n\n-> "), rest)),
-                    ))
-                    .map(|_| Ok(("", Some("".to_string())))),
-                    // When all section types are in place this
-                    // rest.map should be able to be removed. Right
-                    // now it's just catching things that haven't been
-                    // defined yet
-                    rest.map(|x| Ok(("", Some(String::from(x))))),
+                    .map(|_| Ok(("", Some("".to_string())))),)),
+                )),
+                tuple((
+                    multispace0,
+                    tag_no_case("-> "),
+                    alt((
+                        tuple((
+                            tag_no_case("title"),
+                            not_line_ending,
+                            line_ending,
+                            alt((take_until("\n\n-> "), rest)),
+                        ))
+                        .map(|t| self.title_section(t.3)),
+                        tuple((
+                            tag_no_case("p"),
+                            not_line_ending,
+                            line_ending,
+                            alt((take_until("\n\n-> "), rest)),
+                        ))
+                        .map(|t| self.p_section(t.3)),
+                        // When all section types are in place this
+                        // rest.map should be able to be removed. Right
+                        // now it's just catching things that haven't been
+                        // defined yet
+                        rest.map(|x| Ok(("", Some(String::from(x))))),
+                    )),
                 )),
             )),
             eof,
@@ -141,6 +145,25 @@ mod test {
         assert_eq!(
             sf.content(),
             Some(String::from(r#"<h1 class="neo-title">Echo Oscar</h1>"#))
+        );
+    }
+
+    #[test]
+    pub fn ignore_hidden_sections() {
+        let mut sf = SourceFile::new();
+        let lines = vec![
+            "-> title",
+            "",
+            "Tango Whiskey",
+            "",
+            "-> hidden",
+            "",
+            "Pet the dog",
+        ];
+        sf.source_data = Some(lines.join("\n"));
+        assert_eq!(
+            sf.content(),
+            Some(String::from(r#"<h1 class="neo-title">Tango Whiskey</h1>"#))
         );
     }
 
