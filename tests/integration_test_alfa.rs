430a903cdde9996f62ac637be83b7f6b0a804046
Alan Smith <commit@alanwsmith.com>
2023-06-24 21:51:56 -0400

    Stubbed shorthand code snippet test

--- tests/integration_test_alfa.rs
+++ tests/integration_test_alfa.rs
@@ -10,6 +10,7 @@ fn integration_test_alfa() {
     let doc = Html::parse_document(source.as_str());
     let selector = Selector::parse("p").unwrap();
     let mut elements = doc.select(&selector);
+    elements.next();
     assert_eq!(
         elements.next().unwrap().inner_html(),
         "This is the alfa integration test"
@@ -26,19 +27,15 @@ fn integration_test_alfa() {
         elements.next().unwrap().inner_html(),
         r#"Here is <a href="https://www.example.com/">an example</a> link"#
     );
-}
 
-#[test]
-#[ignore]
-fn integration_test_title() {
-    neopolitan::clear_output_directory::clear_output_directory();
-    neopolitan::build_site::build_site();
-    let source = fs::read_to_string("site/posts/integration-test-alfa/index.html").unwrap();
-    let doc = Html::parse_document(source.as_str());
     let selector = Selector::parse("h1").unwrap();
     let mut elements = doc.select(&selector);
     assert_eq!(
         elements.next().unwrap().inner_html(),
         "Integration Test Alfa"
     );
+
+    let selector = Selector::parse("code").unwrap();
+    let mut elements = doc.select(&selector);
+    assert_eq!(elements.next().unwrap().inner_html(), "a snippet");
 }
