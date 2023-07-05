f546675eafbd756a36b187e8daa4cd28dc5bd923
Alan Smith <commit@alanwsmith.com>
2023-07-05 15:24:37 -0400

    Dealing with less than sign that's not a gat

--- src/tags.rs
+++ src/tags.rs
@@ -1,4 +1,5 @@
 use crate::tag_attrs::TagAttr;
+use crate::tags::less_than::less_than;
 use crate::tags::strong::strong;
 use crate::tags::text::text;
 use nom::branch::alt;
@@ -6,18 +7,21 @@ use nom::combinator::eof;
 use nom::multi::many_till;
 use nom::IResult;
 
+pub mod less_than;
 pub mod strong;
 pub mod text;
 
 #[derive(Debug, PartialEq)]
 pub enum Tag {
+    LessThan { text: String },
     Text { text: String },
     Strong { attrs: Vec<TagAttr>, text: String },
 }
 
 pub fn tags(source: &str) -> IResult<&str, Vec<Tag>> {
     dbg!(&source);
-    let (source, snippets) = many_till(alt((strong, text)), eof)(source)?;
+    let (source, snippets) =
+        many_till(alt((less_than, strong, text)), eof)(source)?;
     Ok((source, snippets.0))
 }
 
@@ -48,6 +52,30 @@ mod test {
             },
             Tag::Text {
                 text: " charlie".to_string(),
+            },
+        ];
+        assert_eq!(expected, tags(line).unwrap().1);
+    }
+
+    #[test]
+    pub fn solo_less_than() {
+        let line = "delta <- <<echo|strong>> foxtrot";
+        let expected = vec![
+            Tag::Text {
+                text: "delta ".to_string(),
+            },
+            Tag::LessThan {
+                text: "<-".to_string(),
+            },
+            Tag::Text {
+                text: " ".to_string(),
+            },
+            Tag::Strong {
+                attrs: vec![],
+                text: "echo".to_string(),
+            },
+            Tag::Text {
+                text: " foxtrot".to_string(),
             },
         ];
         assert_eq!(expected, tags(line).unwrap().1);
