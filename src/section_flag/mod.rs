faf01b659968d605dce2028acceefed4eefd0949
Alan Smith <commit@alanwsmith.com>
2025-04-24 21:40:50 -0400

    Moved in flag section metatdata

--- src/section_flag/mod.rs
+++ src/section_flag/mod.rs
@@ -1,22 +1,81 @@
-#![allow(unused)]
 use crate::neo_config::NeoConfig;
-use crate::section_bound::SectionBound;
-use crate::section_category::SectionCategory;
 use crate::section_parent::SectionParent;
-use crate::span_strings::space0_line_ending_or_eof::space0_line_ending_or_eof;
+use nom::IResult;
 use nom::Parser;
+use nom::branch::alt;
 use nom::bytes::complete::is_not;
+use nom::bytes::complete::tag;
+use nom::character::complete::line_ending;
+use nom::character::complete::multispace1;
+use nom::character::complete::space0;
 use nom::character::complete::space1;
+use nom::combinator::eof;
+use nom::combinator::not;
+use nom::multi::many1;
 use nom::sequence::pair;
-use nom::sequence::terminated;
-use nom::{IResult, branch::alt, bytes::complete::tag, combinator::rest};
-use serde::{Deserialize, Serialize};
 
 pub fn section_flag<'a>(
     source: &'a str,
-    config: &'a NeoConfig,
-    parent: &'a SectionParent,
-    debug: bool,
+    _config: &'a NeoConfig,
+    _parent: &'a SectionParent,
+    _debug: bool,
 ) -> IResult<&'a str, String> {
-    Ok(("", "TODO-SECTION-FLAG".to_string()))
+    let (source, _) = tag("--").parse(source)?;
+    let (source, _) = space1.parse(source)?;
+    let (source, parts) = many1(alt((
+        is_not(": \n\t\\"),
+        pair(tag(":"), not(alt((multispace1, eof)))).map(|x| x.0),
+    )))
+    .parse(source)?;
+    let (source, _) =
+        alt((pair(space0, line_ending), pair(space0, eof))).parse(source)?;
+    let value = parts
+        .iter()
+        .map(|part| part.to_string())
+        .collect::<Vec<String>>()
+        .join("");
+    Ok((source, value.to_string()))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("alfa")]
+    #[case("bravo-charlie")]
+    #[case("https://www.example.com/")]
+    #[case("  leading_spaces_are_okay")]
+    #[case("trailing_spaces_are_okay   ")]
+    #[case("these_characters_are_okay:!@#$%^&*()[]<>|-")]
+    fn section_flag_attr_v42_valid_flags(#[case] left: &str) {
+        let config = &NeoConfig::default();
+        let parent = &SectionParent::Basic;
+        let debug = false;
+        let source = format!("-- {}", left);
+        let right = section_flag(&source, config, parent, debug).unwrap().1;
+        assert_eq!(left.trim(), right);
+    }
+
+    #[rstest]
+    #[case("no spaces")]
+    #[case("alfa:")]
+    #[case("bravo: charlie")]
+    #[case("delta: ")]
+    #[case("no_escaped_\\allowed")]
+    fn section_flag_attr_v42_invalid_flags(#[case] left: &str) {
+        let config = &NeoConfig::default();
+        let parent = &SectionParent::Basic;
+        let debug = false;
+        let source = format!("-- {}", left);
+        let right = section_flag(&source, config, parent, debug);
+        if right.is_err() {
+            assert!(true)
+        } else {
+            dbg!(left);
+            assert!(false)
+        }
+    }
 }
--- src/section_metadata/mod.rs
+++ src/section_metadata/mod.rs
@@ -1,13 +1,16 @@
 #![allow(unused)]
 use crate::neo_config::NeoConfig;
 use crate::section_attribute::SectionAttribute;
+use crate::section_attribute::section_attribute;
 use crate::section_bound::SectionBound;
 use crate::section_category::SectionCategory;
+use crate::section_flag::section_flag;
 use crate::section_parent::SectionParent;
 use crate::span_strings::space0_line_ending_or_eof::space0_line_ending_or_eof;
 use nom::Parser;
 use nom::bytes::complete::is_not;
 use nom::character::complete::space1;
+use nom::multi::many0;
 use nom::sequence::pair;
 use nom::sequence::terminated;
 use nom::{IResult, branch::alt, bytes::complete::tag, combinator::rest};
@@ -19,5 +22,25 @@ pub fn section_metadata<'a>(
     parent: &'a SectionParent,
     debug: bool,
 ) -> IResult<&'a str, (Vec<SectionAttribute>, Vec<String>)> {
+    // let (source, raw_metadata) =
+    //     alt((|src| section_flag(src, config, parent, debug),)).parse(source)?;
     Ok((source, (vec![], vec![])))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    #[ignore]
+    pub fn section_metadata_basic_test() {
+        let config = &NeoConfig::default();
+        let source = "-- test-flag\n-- alfa: bravo\n\n";
+        let parent = &SectionParent::Basic;
+        let debug = false;
+        let left = (vec![], vec!["test-flag".to_string()]);
+        let right = section_metadata(source, config, parent, debug).unwrap().1;
+        assert_eq!(left, right);
+    }
 }
