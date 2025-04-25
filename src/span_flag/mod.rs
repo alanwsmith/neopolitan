51e9616cbadd2c32630b22938e0e15f74d8934ce
Alan Smith <commit@alanwsmith.com>
2025-04-25 15:51:43 -0400

    Cleanup and removed escape character

--- src/span_flag/mod.rs
+++ src/span_flag/mod.rs
@@ -1,44 +1,20 @@
-#![allow(unused)]
-use crate::neo_config::NeoConfig;
-use crate::section_flag::raw_section_flag;
-use crate::section_parent::SectionParent;
-use crate::span::Span;
-use crate::span_metadata::RawShorthandMetadata;
 use crate::span_metadata::RawShorthandMetadataDev;
-use crate::span_strings::escaped_character::escaped_character;
-use crate::span_strings::plain_text_single_line_ending_as_space::plain_text_single_line_ending_as_space;
-use crate::span_strings::plain_text_space1_as_single_space::plain_text_space1_as_single_space;
-use crate::span_strings::plain_text_string_base::plain_text_string_base;
-use crate::span_strings::single_character;
 use crate::span_strings::single_character::single_character;
 use nom::IResult;
 use nom::Parser;
 use nom::branch::alt;
-use nom::bytes::complete::is_a;
 use nom::bytes::complete::is_not;
 use nom::bytes::complete::tag;
-use nom::bytes::complete::take;
-use nom::character::complete::alphanumeric1;
-use nom::character::complete::char;
 use nom::character::complete::line_ending;
 use nom::character::complete::one_of;
-use nom::character::complete::satisfy;
 use nom::character::complete::space0;
-use nom::character::complete::space1;
-use nom::combinator::eof;
-use nom::combinator::fail;
 use nom::combinator::not;
 use nom::combinator::opt;
 use nom::combinator::peek;
 use nom::combinator::recognize;
-use nom::multi::many0;
 use nom::multi::many1;
-use nom::sequence::pair;
 use nom::sequence::preceded;
 use nom::sequence::terminated;
-use serde::Deserialize;
-use serde::Serialize;
-use std::collections::BTreeMap;
 
 // NOTE: Empty spaces is trimmed from
 // around the flag including newlines.
@@ -65,11 +41,9 @@ pub fn not_character<'a>(
     source: &'a str,
     character: &'a str,
 ) -> IResult<&'a str, &'a str> {
-    let (source, result) = recognize(preceded(
-        not(tag(character)),
-        one_of("~!@#$%^&*()<<>>[[]]{{}}"),
-    ))
-    .parse(source)?;
+    let (source, result) =
+        recognize(preceded(not(tag(character)), one_of("`~!@#$%^&*()<>[]{}")))
+            .parse(source)?;
     Ok((source, result))
 }
 
@@ -80,7 +54,7 @@ pub fn span_flag<'a>(
     let (source, _) =
         (tag("|"), space0, opt(line_ending), space0).parse(source)?;
     let (source, spans) = many1(alt((
-        is_not(" \r\n\t|~`!@#$%^&*()<<>>[[]]{{}}"),
+        is_not(" \r\n\t\\|~`!@#$%^&*()<>[]{}"),
         single_character,
         |src| not_character(src, character),
     )))
@@ -122,6 +96,8 @@ mod test {
         "https://www.example.com/",
         "``"
     )]
+    #[case("|alfa-bravo``", "`", "alfa-bravo", "``")]
+    #[case("|alfa_bravo``", "`", "alfa_bravo", "``")]
     #[case("|single`character``", "`", "single`character", "``")]
     #[case("|alfa|", "`", "alfa", "|")]
     #[case("|alfa |", "`", "alfa", "|")]
@@ -132,6 +108,18 @@ mod test {
         "`",
         "others~~!!@@##$$%%^^&&**(())<<>>[[]]{{}}",
         "``"
+    )]
+    #[case(
+        "|others``~~!!@@##$$%%^^&&**(())<<[[]]{{}}>>",
+        ">",
+        "others``~~!!@@##$$%%^^&&**(())<<[[]]{{}}",
+        ">>"
+    )]
+    #[case(
+        "|others``~~!!@@##$$%%^^&&(())<<>>[[]]{{}}**",
+        "*",
+        "others``~~!!@@##$$%%^^&&(())<<>>[[]]{{}}",
+        "**"
     )]
     fn span_flag_valid_tests(
         #[case] source: &str,
@@ -145,19 +133,20 @@ mod test {
         assert_eq!(remainder, right.0);
     }
 
-    // #[rstest]
-    // #[case("|alfa bravo", "`")]
-    // fn span_flag_invalid_tests(#[case] source: &str, #[case] character: &str) {
-    //     let characters = "%@~*^![]{}<>_#:".to_string();
-    //     let result = span_flag(source, character);
-    //     match result {
-    //         Ok(_) => {
-    //             dbg!(result);
-    //             assert!(false)
-    //         }
-    //         Err(_) => assert!(true),
-    //     }
-    // }
+    #[rstest]
+    #[case("|alfa bravo``", "`")]
+    #[case("|alfa: bravo``", "`")]
+    #[case("|alfa\\bravo``", "`")]
+    fn span_flag_invalid_tests(#[case] source: &str, #[case] character: &str) {
+        let result = span_flag(source, character);
+        match result {
+            Ok(_) => {
+                dbg!(result);
+                assert!(false)
+            }
+            Err(_) => assert!(true),
+        }
+    }
 
     //
 }
