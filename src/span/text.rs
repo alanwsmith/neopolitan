4362ee0ba5d6e4d9a3355d8f708fd5da4654529b
Alan Smith <commit@alanwsmith.com>
2025-04-25 16:24:33 -0400

    Added tests for Span::Text

--- src/span/text.rs
+++ src/span/text.rs
@@ -6,6 +6,7 @@ use crate::section_parent::SectionParent;
 use crate::span::Span;
 use crate::span_metadata::span_metadata;
 use crate::span_strings::escaped_character::escaped_character;
+use crate::span_strings::plain_text_any_colons::plain_text_any_colons;
 use crate::span_strings::plain_text_single_line_ending_as_space::plain_text_single_line_ending_as_space;
 use crate::span_strings::plain_text_space1_as_single_space::plain_text_space1_as_single_space;
 use crate::span_strings::plain_text_string_base::plain_text_string_base;
@@ -30,31 +31,64 @@ use serde::Deserialize;
 use serde::Serialize;
 use std::collections::BTreeMap;
 
-pub fn text_span<'a>(
-    source: &'a str,
-    start_marker: &'a str,
-    end_marker: &'a str,
-) -> IResult<&'a str, Span> {
-    let (source, tokens) = preceded(
-        pair(
-            pair(tag(start_marker), tag(start_marker)),
-            opt(plain_text_space1_as_single_space),
-        ),
-        many1(alt((
-            plain_text_string_base,
-            plain_text_space1_as_single_space,
-            is_a("%@~*^![]{}<>_#:"),
-            plain_text_single_line_ending_as_space,
-            escaped_character,
-        ))),
-    )
+pub fn text_span<'a>(source: &'a str) -> IResult<&'a str, Span> {
+    let (source, results) = many1(alt((
+        plain_text_string_base,
+        plain_text_space1_as_single_space,
+        plain_text_single_line_ending_as_space,
+        plain_text_any_colons,
+    )))
     .parse(source)?;
-    //let (source, (flags, attrs)) = span_metadata(source, characters.clone())?;
-    let (source, _) = tag("``").parse(source)?;
     Ok((
         source,
         Span::Text {
-            content: "asdf".to_string(),
+            content: results.join("").to_string(),
         },
     ))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("alfa", Span::Text{ content: "alfa".to_string()}, "")]
+    #[case("alfa bravo", Span::Text{ content: "alfa bravo".to_string()}, "")]
+    #[case("alfa \"bravo\"", Span::Text{ content: "alfa \"bravo\"".to_string()}, "")]
+    #[case("alfa` bravo", Span::Text{ content: "alfa` bravo".to_string()}, "")]
+    #[case("alfa~ bravo", Span::Text{ content: "alfa~ bravo".to_string()}, "")]
+    #[case("alfa\nbravo", Span::Text{ content: "alfa bravo".to_string()}, "")]
+    #[case("alfa \nbravo", Span::Text{ content: "alfa bravo".to_string()}, "")]
+    #[case("alfa\n\nbravo", Span::Text{ content: "alfa".to_string()}, "\n\nbravo")]
+    #[case("https://www.example.com/", Span::Text{ content: "https://www.example.com/".to_string()}, "")]
+    #[case("alfa bravo -\n- charlie delta", Span::Text{ content: "alfa bravo - - charlie delta".to_string()}, "")]
+    #[case("alfa^^1^^", Span::Text{ content: "alfa".to_string()}, "^^1^^")]
+    #[case("alfa <<span|ping>>", Span::Text{ content: "alfa ".to_string()}, "<<span|ping>>")]
+    fn text_span_valid_tests(
+        #[case] source: &str,
+        #[case] left: Span,
+        #[case] remainder: &str,
+    ) {
+        let right = text_span(source).unwrap();
+        assert_eq!(left, right.1);
+        assert_eq!(remainder, right.0);
+    }
+
+    #[rstest]
+    #[case("``alfa")]
+    #[case("<<alfa")]
+    fn text_span_invalid_tests(#[case] source: &str) {
+        let result = text_span(source);
+        match result {
+            Ok(_) => {
+                dbg!(result.unwrap());
+                assert!(false)
+            }
+            Err(_) => assert!(true),
+        }
+    }
+
+    //
 }
--- src/span_attr/mod.rs
+++ src/span_attr/mod.rs
@@ -3,11 +3,13 @@ use crate::neo_config::NeoConfig;
 use crate::section_flag::raw_section_flag;
 use crate::section_parent::SectionParent;
 use crate::span::Span;
+use crate::span::text::text_span;
 use crate::span_metadata::RawSpanMetadata;
 use crate::span_strings::escaped_character::escaped_character;
 use crate::span_strings::plain_text_single_line_ending_as_space::plain_text_single_line_ending_as_space;
 use crate::span_strings::plain_text_space1_as_single_space::plain_text_space1_as_single_space;
 use crate::span_strings::plain_text_string_base::plain_text_string_base;
+use crate::span_strings::single_character::single_character;
 use nom::IResult;
 use nom::Parser;
 use nom::branch::alt;
@@ -16,10 +18,12 @@ use nom::bytes::complete::is_not;
 use nom::bytes::complete::tag;
 use nom::character::complete::alphanumeric1;
 use nom::character::complete::line_ending;
+use nom::character::complete::one_of;
 use nom::character::complete::space0;
 use nom::character::complete::space1;
 use nom::combinator::not;
 use nom::combinator::opt;
+use nom::combinator::recognize;
 use nom::multi::many0;
 use nom::multi::many1;
 use nom::sequence::pair;
@@ -29,9 +33,26 @@ use serde::Deserialize;
 use serde::Serialize;
 use std::collections::BTreeMap;
 
-pub fn span_attr(source: &str) -> IResult<&str, RawSpanMetadata> {
+pub fn not_character<'a>(
+    source: &'a str,
+    character: &'a str,
+) -> IResult<&'a str, &'a str> {
+    let (source, result) =
+        recognize(preceded(not(tag(character)), one_of("`~!@#$%^&*()<>[]{}")))
+            .parse(source)?;
+    Ok((source, result))
+}
+
+pub fn span_attr<'a>(
+    source: &'a str,
+    character: &'a str,
+) -> IResult<&'a str, RawSpanMetadata> {
+    let (source, spans) =
+        (tag("|"), space0, opt(line_ending), space0).parse(source)?;
+    let (source, spans) = many1(alt((text_span,))).parse(source)?;
+
     Ok((
-        "",
+        "``",
         RawSpanMetadata::Attr {
             key: "alfa".to_string(),
             spans: vec![],
@@ -50,8 +71,11 @@ mod test {
     fn span_flag_valid_tests(
         #[case] source: &str,
         #[case] character: &str,
-        #[case] found: RawSpanMetadata,
+        #[case] left: RawSpanMetadata,
         #[case] remainder: &str,
     ) {
+        let right = span_attr(source, character).unwrap();
+        assert_eq!(left, right.1);
+        assert_eq!(remainder, right.0);
     }
 }
