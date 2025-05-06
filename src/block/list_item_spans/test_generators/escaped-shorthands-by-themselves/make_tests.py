0838bc6a6e114e03e9aef8a75926d2b575c84cf3
Alan Smith <commit@alanwsmith.com>
2025-05-06 14:08:04 -0400

    Moving generated tests to their own parent folders

---
+++ src/block/list_item_spans/test_generators/escaped-shorthands-by-themselves/make_tests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import sys
+
+from string import Template
+from pathlib import Path 
+
+data_file = "../../../../helpers/data/characters-to-escape-in-blocks.txt"
+output_root = "../../tests/generated-ok"
+test_name = os.path.basename(sys.path[0])
+
+with open(data_file, "r") as _char_file:
+    lines = _char_file.read().splitlines()
+    for line in lines:
+        parts = line.split(" ")
+        if len(parts) >= 2:
+            payload = {
+                    "NAME": parts[1],
+                    "TOKEN": parts[0]
+                    }
+            with open(f"template.neotest", "r") as _tmpl:
+                output_dir = f"{output_root}/{test_name}"
+                Path(output_dir).mkdir(exist_ok=True)
+                output_path = f"{output_dir}/{payload["NAME"]}.neotest"
+                print(output_path)
+                template = Template(_tmpl.read())
+                output = template.substitute(payload)
+                with open(output_path, "w") as _out:
+                    _out.write(output)
+
