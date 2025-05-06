48d2e4160c8871ffe2dc16b2405c43a1b24b677c
Alan Smith <commit@alanwsmith.com>
2025-05-05 23:42:48 -0400

    Set up to make automatically generated test for single shorthands

--- src/span/list_item_text_span/tests/generators/valid_shorthands/make_single_shorthands.py
+++ src/span/list_item_text_span/tests/generators/valid_shorthands/make_single_shorthands.py
@@ -10,7 +10,7 @@ single_shorthands = [
         ]
 
 template_dir = "single_shorthands"
-output_dir = "../../tests/valid"
+output_dir = "../../../tests/valid"
 
 file_names = [
     os.path.basename(file).split(".")[0] for file in glob.glob(f"{template_dir}/*")
@@ -20,15 +20,11 @@ file_names = [
 for shorthand in single_shorthands:
     for file_name in file_names:
         with open(f"{template_dir}/{file_name}.tmpl", "r") as _tmpl:
-            output_path = f"{output_dir}/{shorthand["NAME"]}-{file_name}.txt"
+            output_path = f"{output_dir}/generated-{shorthand["NAME"]}-{file_name}.txt"
             print(output_path)
-
-            tmpl = _tmpl.read()
-            print(tmpl)
-
-
-
-# for shorthand in shorthands: 
-#     print("asdf")
+            template = Template(_tmpl.read())
+            output = template.substitute(shorthand)
+            with open(output_path, "w") as _out:
+                _out.write(output)
 
 
