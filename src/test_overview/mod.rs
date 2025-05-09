db8bc60d8e59542ac185f6bea26981e76840d29b
Alan Smith <commit@alanwsmith.com>
2025-05-09 01:10:46 -0400

    Getting first level test-overview output

--- src/test_overview/mod.rs
+++ src/test_overview/mod.rs
@@ -8,7 +8,8 @@ use anyhow::Error;
 use anyhow::Result;
 use anyhow::anyhow;
 use itertools::Itertools;
-use minijinja::{Environment, context};
+use minijinja::syntax::SyntaxConfig;
+use minijinja::{Environment, Value, context};
 use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 use std::fs;
@@ -33,6 +34,32 @@ impl TestOverview<'_> {
     //     Ok(())
     // }
 
+    pub fn load_templates(&mut self) -> Result<()> {
+        &self
+            .env
+            .add_template(
+                "index",
+                r#"-- title
+
+Test Overview
+
+-- html
+
+<div><a href="..">Up</a></div>
+<ul>
+    [!- for link in links -!]
+        <li>
+        <a href="[@ link @]/">[@ link @]</a>
+        </li>
+    [!- endfor -!]
+</ul>
+"#,
+            )
+            .unwrap();
+
+        Ok(())
+    }
+
     pub fn make_output_dirs(&self) -> Result<()> {
         for output_dir in &self.output_dirs() {
             std::fs::create_dir_all(output_dir)?;
@@ -44,7 +71,15 @@ impl TestOverview<'_> {
         input_root: &'a PathBuf,
         output_root: &'a PathBuf,
     ) -> TestOverview<'a> {
-        let env = Environment::new();
+        let mut env = Environment::new();
+        env.set_syntax(
+            SyntaxConfig::builder()
+                .block_delimiters("[!", "!]")
+                .variable_delimiters("[@", "@]")
+                .comment_delimiters("[#", "#]")
+                .build()
+                .unwrap(),
+        );
         let file_list =
             get_file_list(input_root, &vec!["neotest".to_string()]).unwrap();
         let cases =
@@ -68,10 +103,22 @@ impl TestOverview<'_> {
             .collect()
     }
 
-    pub fn output_index_files(&self, working_dir: &PathBuf) -> Result<()> {
-        let child_dirs = get_dirs_in_dir(&working_dir)?;
-        dbg!(child_dirs);
-
+    pub fn output_index_files(&mut self, working_dir: &PathBuf) -> Result<()> {
+        //let template_string = self.index_template();
+        //self.env.add_template("index", template_string);
+        // .add_template_owned("index", self.index_template().to_string());
+        // &self.env.add_template_owned("index", "asdf".to_string());
+        let file_output = working_dir.join("index.neo");
+        dbg!(&file_output);
+        let child_dirs: Vec<PathBuf> = get_dirs_in_dir(&working_dir)?
+            .iter()
+            .filter_map(|p| p.strip_prefix(&self.output_root).ok())
+            .map(|p| p.to_path_buf())
+            .collect();
+        let links = Value::from_serialize(child_dirs);
+        let template = self.env.get_template("index")?;
+        let output = template.render(context![links])?;
+        fs::write(file_output, output)?;
         Ok(())
     }
 }
@@ -84,9 +131,9 @@ mod test {
     fn solo_make_report() {
         let input_root = PathBuf::from("src");
         let output_root = PathBuf::from("docs-content/_test-overview");
-        let tr = TestOverview::new(&input_root, &output_root);
+        let mut tr = TestOverview::new(&input_root, &output_root);
+        tr.load_templates();
         tr.make_output_dirs();
-        let _ = tr.output_index_files(&tr.output_root);
-        assert!(false);
+        tr.output_index_files(&tr.output_root.clone());
     }
 }
