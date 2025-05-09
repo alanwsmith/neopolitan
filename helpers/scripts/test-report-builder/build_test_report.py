55d1bced10753a9247f5ac8051ec14c07de43938
Alan Smith <commit@alanwsmith.com>
2025-05-08 22:01:53 -0400

    Outputting basic index pages

--- helpers/scripts/test-report-builder/build_test_report.py
+++ helpers/scripts/test-report-builder/build_test_report.py
@@ -10,6 +10,7 @@ class ReportMaker:
         self.input_root = input_root 
         self.output_root = output_root
         self.file_path_parts = []
+        self.dir_tree = {}
 
     def get_file_list(self):
         for root, dirs, files in os.walk(self.input_root):
@@ -21,16 +22,56 @@ class ReportMaker:
                     self.file_path_parts.append(file_parts)
 
     def make_output_dirs(self):
+        for key in self.dir_tree:
+            self.make_output_dir(key, self.dir_tree[key])
+
+    def make_output_dir(self, key, next):
+        output_dir = os.path.join(self.output_root, key)
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
+        output_index = os.path.join(output_dir, "index.neo")
+        with open(output_index, "w") as _out_in:
+            _out_in.write(f"""-- title\n\n{key}\n\n""")
+            _out_in.write(f"""-- html\n\n<div><a href="../">Up</a></div>""")
+            for new_key in sorted(next):
+                send_key = os.path.join(key, new_key)
+                _out_in.write(f"""<div class="padding-medium"><a href="{new_key}">{new_key}</a></div>""")
+                self.make_output_dir(send_key, next[new_key])
+                # print(send_key)
+
+        # print(output_dir)
+        # print(next)
+        #print(next)
+
+        # for path_parts in self.file_path_parts: 
+        #     rel_dir = "/".join(path_parts[4:-1])
+        #     output_dir = os.path.join(self.output_root, rel_dir)
+        #     print(output_dir)
+
+    def load_dir_tree(self):
         for path_parts in self.file_path_parts: 
-            rel_dir = "/".join(path_parts[4:-1])
-            output_dir = os.path.join(self.output_root, rel_dir)
-            print(output_dir)
-            Path(output_dir).mkdir(parents=True, exist_ok=True)
+            sub_parts = path_parts[4:-1]
+            self.add_to_tree(self.dir_tree, sub_parts)
+
+    def add_to_tree(self, current, chain):
+        link = chain.pop(0)
+        if link not in current:
+            current[link] = {}
+        if len(chain) > 0:
+            self.add_to_tree(current[link], chain)
 
 
 
 
+        # if chain[link_index] not in self.dir_tree:
+        #     self.dir_tree[chain[link_index]] = {}
+        #     if len(chain) > link_index:
+        #         self.add_to_tree
+        #         print(len(chain))
 
+
+            # for sub_part in sub_parts:
+            #     if sub_part not in self.dir_tree:
+            #         self.dir_tree[sub_part] = {}
 
 
 if __name__ == "__main__": 
@@ -39,8 +80,10 @@ if __name__ == "__main__":
             "../../../docs-content/_test-cases"
         )
     maker.get_file_list()
-#    print(maker.file_list)
+    maker.load_dir_tree()
     maker.make_output_dirs()
+
+    #print(maker.dir_tree)
 
 # file_list = []
 # for root, dirs, files in os.walk("../../.."):
